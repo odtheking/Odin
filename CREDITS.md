7495a60fffcb14ac3ed73e8926aba9ca4bfaa69a
freebonsai <118639445+freebonsai@users.noreply.github.com>
2023-06-23 16:13:31 +0200

    Start

---
+++ CREDITS.md
@@ -0,0 +1,9 @@
+We would like to express our gratitude to the following open-source projects, who have greatly aided us in the development of this mod:
+
+- [Floppa Client](https://github.com/FloppaCoding/FloppaClient)
+- [Dulkir Mod](https://github.com/inglettronald/DulkirMod)
+- [Skyblock Addons](https://github.com/BiscuitDevelopment/SkyblockAddons)
+- [Skyblock Client](https://github.com/Harry282/Skyblock-Client)
+- [Skytils](https://github.com/Skytils/SkytilsMod)
+
+Please feel free to use this project as inspiration, and copy parts of it, although credits are always much appreciated!
---
+++ build.gradle.kts
@@ -0,0 +1,145 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    idea
+    java
+    id("gg.essential.loom") version "0.10.0.+"
+    id("dev.architectury.architectury-pack200") version "0.1.3"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    kotlin("jvm") version "1.8.20"
+}
+
+group = "com.example.archloomtemplate"
+version = "1.0.0"
+
+// Toolchains:
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+}
+
+// Minecraft configuration:
+loom {
+    log4jConfigs.from(file("log4j2.xml"))
+    launchConfigs {
+        "client" {
+            // If you don't want mixins, remove these lines
+            property("mixin.debug", "true")
+            property("asmhelper.verbose", "true")
+            arg("--tweakClass", "cc.polyfrost.oneconfig.loader.stage0.LaunchWrapperTweaker")
+            arg("--mixin", "mixins.odinclient.json")
+        }
+    }
+    forge {
+        pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
+        // If you don't want mixins, remove this lines
+        mixinConfig("mixins.odinclient.json")
+    }
+    // If you don't want mixins, remove these lines
+    mixin {
+        defaultRefmapName.set("mixins.odinclient.refmap.json")
+    }
+}
+
+sourceSets.main {
+    java.srcDir(file("$projectDir/src/main/kotlin"))
+    output.setResourcesDir(file("$buildDir/classes/java/main"))
+}
+
+val packageLib by configurations.creating {
+    configurations.implementation.get().extendsFrom(this)
+}
+
+// Dependencies:
+
+repositories {
+    mavenCentral()
+    maven("https://repo.spongepowered.org/maven/")
+    // If you don't want to log in with your real minecraft account, remove this line
+    maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
+    maven("https://repo.essential.gg/repository/maven-public/")
+    maven("https://repo.polyfrost.cc/releases")
+}
+
+val shadowImpl: Configuration by configurations.creating {
+    configurations.implementation.get().extendsFrom(this)
+}
+
+
+
+dependencies {
+    minecraft("com.mojang:minecraft:1.8.9")
+    mappings("de.oceanlabs.mcp:mcp_stable:22-1.8.9")
+    forge("net.minecraftforge:forge:1.8.9-11.15.1.2318-1.8.9")
+
+    implementation(kotlin("stdlib-jdk8"))
+
+    // If you don't want mixins, remove these lines
+    shadowImpl("org.spongepowered:mixin:0.7.11-SNAPSHOT") {
+        isTransitive = false
+    }
+    annotationProcessor("org.spongepowered:mixin:0.8.5-SNAPSHOT:processor")
+
+    // If you don't want to log in with your real minecraft account, remove this line
+    runtimeOnly("me.djtheredstoner:DevAuth-forge-legacy:1.1.0")
+
+    // Basic OneConfig dependencies for legacy versions. See OneConfig example mod for more info
+    compileOnly("cc.polyfrost:oneconfig-1.8.9-forge:0.2.0-alpha+") // Should not be included in jar
+    // include should be replaced with a configuration that includes this in the jar
+    shadowImpl("cc.polyfrost:oneconfig-wrapper-launchwrapper:1.0.0-beta+") // Should be included in jar
+
+}
+
+// Configures our shadow/shade configuration, so we can
+// include some dependencies within our mod jar file.
+tasks.named<ShadowJar>("shadowJar") {
+    archiveClassifier.set("dev") // TODO: machete gets confused by the `dev` prefix.
+    configurations = listOf(shadowImpl)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+// Tasks:
+
+tasks.withType(JavaCompile::class) {
+    options.encoding = "UTF-8"
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.withType(Jar::class) {
+    archiveBaseName.set("odinclient")
+    manifest.attributes.run {
+        this["FMLCorePluginContainsFMLMod"] = "true"
+        this["ForceLoadAsMod"] = "true"
+
+        // If you don't want mixins, remove these lines
+        this["TweakClass"] = "cc.polyfrost.oneconfig.loader.stage0.LaunchWrapperTweaker"
+        this["MixinConfigs"] = "mixins.odinclient.json"
+    }
+}
+
+
+val remapJar by tasks.named<net.fabricmc.loom.task.RemapJarTask>("remapJar") {
+    archiveClassifier.set("all")
+    from(tasks.shadowJar)
+    input.set(tasks.shadowJar.get().archiveFile)
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("all-dev")
+    configurations = listOf(shadowImpl)
+    doLast {
+        configurations.forEach {
+            println("Config: ${it.files}")
+        }
+    }
+
+    // If you want to include other dependencies and shadow them, you can relocate them in here
+    fun relocate(name: String) = relocate(name, "com.odinclient.deps.$name")
+}
+
+tasks.withType<Jar> { duplicatesStrategy = DuplicatesStrategy.EXCLUDE }
+
+tasks.assemble.get().dependsOn(tasks.remapJar)
