a782ee0ed5c1abc6dab0fe5a913ce37a09086761
freebonsai <118639445+freebonsai@users.noreply.github.com>
2023-08-09 15:00:42 +0200

    Needs to be PlayerInteractEvent

--- src/main/kotlin/me/odinclient/features/impl/dungeon/ThornStun.kt
+++ src/main/kotlin/me/odinclient/features/impl/dungeon/ThornStun.kt
@@ -1,9 +1,7 @@
 package me.odinclient.features.impl.dungeon
 
-import me.odinclient.OdinClient
 import me.odinclient.features.Category
 import me.odinclient.features.Module
-import me.odinclient.features.impl.m7.DioriteFucker
 import me.odinclient.features.settings.impl.NumberSetting
 import me.odinclient.utils.Utils.equalsOneOf
 import me.odinclient.utils.skyblock.ChatUtils
@@ -11,8 +9,7 @@ import me.odinclient.utils.skyblock.ItemUtils.heldItem
 import me.odinclient.utils.skyblock.ItemUtils.itemID
 import me.odinclient.utils.skyblock.PlayerUtils.rightClick
 import me.odinclient.utils.skyblock.dungeon.DungeonUtils
-import net.minecraft.init.Blocks
-import net.minecraftforge.client.event.MouseEvent
+import net.minecraftforge.event.entity.player.PlayerInteractEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 object ThornStun : Module(
@@ -25,8 +22,12 @@ object ThornStun : Module(
     private val delay: Long by NumberSetting("Delay", 250, 100, 1000, 10)
 
     @SubscribeEvent
-    fun onMouse(event: MouseEvent) {
-        if (DungeonUtils.isFloor(4) || event.button != 1 || event.buttonstate || !heldItem?.itemID.equalsOneOf("TRIBAL_SPEAR", "BONE_BOOMERANG")) return
+    fun onInteract(event: PlayerInteractEvent) {
+        if (
+            DungeonUtils.isFloor(4) ||
+            !event.action.equalsOneOf(PlayerInteractEvent.Action.RIGHT_CLICK_AIR, PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) ||
+            !heldItem?.itemID.equalsOneOf("TRIBAL_SPEAR", "BONE_BOOMERANG")
+        ) return
         event.isCanceled = true
         isClicking = !isClicking
         ChatUtils.modMessage("Thorn Stun: ${if (isClicking) "§aClicking!" else "§cNot Clicking."}")
