1b42c9142dbb0c9d49449dd73e33c61d1a7006fd
Cezqr <Cezqr1337@gmail.com>
2023-08-21 16:41:24 +0200

    Add Ghost Triggerbot

--- src/main/kotlin/me/odinclient/features/impl/dungeon/Triggerbot.kt
+++ src/main/kotlin/me/odinclient/features/impl/dungeon/Triggerbot.kt
@@ -4,26 +4,46 @@ import me.odinclient.OdinClient.Companion.mc
 import me.odinclient.features.Category
 import me.odinclient.features.Module
 import me.odinclient.features.settings.impl.BooleanSetting
+import me.odinclient.mixin.MinecraftAccessor
 import me.odinclient.utils.VecUtils
+import me.odinclient.utils.render.world.RenderUtils
 import me.odinclient.utils.skyblock.PlayerUtils
 import me.odinclient.utils.skyblock.dungeon.DungeonUtils
 import net.minecraft.client.entity.EntityOtherPlayerMP
+import net.minecraft.entity.SharedMonsterAttributes
+import net.minecraft.entity.monster.EntityCreeper
 import net.minecraft.util.AxisAlignedBB
 import net.minecraftforge.event.entity.EntityJoinWorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent
 
 object Triggerbot : Module(
     name = "Triggerbot",
-    description = "Instantly left clicks if you are looking at a spirit bear or blood mob when they spawn",
+    description = "Instantly left clicks if you are looking a specified mob",
     category = Category.DUNGEON
 ) {
-    private val bloodTriggerbot: Boolean by BooleanSetting("Blood Triggerbot", false)
-    private val spiritBearTriggerbot: Boolean by BooleanSetting("Spirit Bear Triggerbot", false)
+    private val blood: Boolean by BooleanSetting("Blood Mobs")
+    private val spiritBear: Boolean by BooleanSetting("Spirit Bear")
+    private val ghosts: Boolean by BooleanSetting("Ghosts")
 
     private val bloodMobs: Set<String> = setOf(
         "Revoker", "Psycho", "Reaper", "Cannibal", "Mute", "Ooze", "Putrid", "Freak", "Leech", "Tear",
         "Parasite", "Flamer", "Skull", "Mr.Dead", "Vader", "Frost", "Walker", "Bonzo", "Scarf", "Livid", "WanderingSoul"
     )
+
+    @SubscribeEvent
+    fun onTick(event: ClientTickEvent)
+    {
+        val lookingAt = mc.objectMouseOver.entityHit ?: return
+        if (mc.thePlayer.getPositionEyes(RenderUtils.partialTicks).distanceTo(mc.objectMouseOver.hitVec) <= 3)
+        {
+            val creeperMaxHealth = (lookingAt as EntityCreeper).getEntityAttribute(SharedMonsterAttributes.maxHealth).baseValue
+            if ((ghosts && (creeperMaxHealth == 1000000.0 || creeperMaxHealth == 4000000.0)))
+            {
+                (mc as MinecraftAccessor).invokeClickMouse()
+            }
+        }
+    }
 
     @SubscribeEvent
     fun onEntityJoin(event: EntityJoinWorldEvent) {
@@ -35,8 +55,8 @@ object Triggerbot : Module(
         val ent = event.entity
         val name = ent.name.replace(" ", "")
         if (
-            !(bloodMobs.contains(name) && bloodTriggerbot) &&
-            !(name == "Spirit Bear" && spiritBearTriggerbot)
+            !(bloodMobs.contains(name) && blood) &&
+            !(name == "Spirit Bear" && spiritBear)
         ) return
 
         if (
