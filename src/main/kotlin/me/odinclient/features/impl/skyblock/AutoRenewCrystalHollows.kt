8c8279c07c2e5d0c785548960889061242200c53
freebonsai <118639445+freebonsai@users.noreply.github.com>
2023-08-30 21:54:03 +0200

    b

--- src/main/kotlin/me/odinclient/features/impl/skyblock/AutoRenewCrystalHollows.kt
+++ src/main/kotlin/me/odinclient/features/impl/skyblock/AutoRenewCrystalHollows.kt
@@ -2,6 +2,7 @@ package me.odinclient.features.impl.skyblock
 
 import me.odinclient.features.Category
 import me.odinclient.features.Module
+import me.odinclient.utils.skyblock.ChatUtils
 import net.minecraft.client.Minecraft
 import net.minecraftforge.client.event.ClientChatReceivedEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -11,10 +12,9 @@ object AutoRenewCrystalHollows : Module(
     category = Category.SKYBLOCK
 ) {
 
-    @SubscribeEvent
-    fun onChat(event: ClientChatReceivedEvent) {
-        if(event.message.unformattedText.equals("Your pass to the Crystal Hollows will expire in 1 minute")) {
-            Minecraft.getMinecraft().thePlayer.sendChatMessage("/purchasecrystallhollowspass");
+    init {
+        onMessage("Your pass to the Crystal Hollows will expire in 1 minute".toRegex(), { enabled }) {
+            ChatUtils.sendCommand("purchasecrystallhollowspass");
         }
     }
 
