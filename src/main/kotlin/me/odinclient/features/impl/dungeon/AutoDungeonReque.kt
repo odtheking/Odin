8371247e28ca6d40a02f8a2169509b65dc6dfa61
freebonsai <118639445+freebonsai@users.noreply.github.com>
2023-09-01 11:27:34 +0200

    add delay in AutoDungeonReque

--- src/main/kotlin/me/odinclient/features/impl/dungeon/AutoDungeonReque.kt
+++ src/main/kotlin/me/odinclient/features/impl/dungeon/AutoDungeonReque.kt
@@ -1,8 +1,12 @@
 package me.odinclient.features.impl.dungeon
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import me.odinclient.OdinClient.Companion.scope
 import me.odinclient.events.impl.ChatPacketEvent
 import me.odinclient.features.Category
 import me.odinclient.features.Module
+import me.odinclient.features.settings.impl.NumberSetting
 import me.odinclient.utils.skyblock.ChatUtils
 import me.odinclient.utils.skyblock.dungeon.DungeonUtils
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -13,11 +17,13 @@ object AutoDungeonReque : Module(
     category = Category.DUNGEON,
     tag = TagType.NEW
 ) {
+    private val delay: Int by NumberSetting("Delay", 10, 0, 30, 1)
 
     @SubscribeEvent
     fun onChat(event: ChatPacketEvent) {
-        if (!DungeonUtils.inDungeons) return
-        if (event.message == "                             > EXTRA STATS <") {
+        if (!DungeonUtils.inDungeons || event.message != "                             > EXTRA STATS <") return
+        scope.launch {
+            delay(delay * 1000L)
             ChatUtils.sendCommand("instancerequeue")
         }
     }
