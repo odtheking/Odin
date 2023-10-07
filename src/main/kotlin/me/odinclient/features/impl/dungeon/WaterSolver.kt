1a3d0d0c9dddeeddb4514e4e6dcc678036a14d9b
freebonsai <118639445+freebonsai@users.noreply.github.com>
2023-10-07 20:57:47 +0200

    cleaned up WaterSolver a bit

--- src/main/kotlin/me/odinclient/features/impl/dungeon/WaterSolver.kt
+++ src/main/kotlin/me/odinclient/features/impl/dungeon/WaterSolver.kt
@@ -44,58 +44,52 @@ object WaterSolver : Module(
     private var inWaterRoom = false
     private var variant = -1
     private var extendedSlots = ""
-    private var ticks = 0
     private var solutions = mutableMapOf<LeverBlock, Array<Double>>()
     private var openedWater = -1L
-    private var job: Job? = null
 
-    @SubscribeEvent
-    fun onClientTick(event: TickEvent.ClientTickEvent) {
-        if (event.phase == TickEvent.Phase.END || !DungeonUtils.inDungeons) return
-        val player = Minecraft.getMinecraft().thePlayer ?: return
-        val world = Minecraft.getMinecraft().theWorld ?: return
-
-        if (ticks % 20 == 0) {
-            if (variant == -1 && (job == null || job?.isCancelled == true || job?.isCompleted == true)) {
-                job = ModCore.scope.launch {
-                    prevInWaterRoom = inWaterRoom
-                    inWaterRoom = false
-                    if (BlockPos.getAllInBox(
-                            BlockPos(player.posX.toInt() - 13, 54, player.posZ.toInt() - 13),
-                            BlockPos(player.posX.toInt() + 13, 54, player.posZ.toInt() + 13))
-                            .any { world.getBlockState(it).block == Blocks.sticky_piston }) {
-                        val xRange = player.posX.toInt() - 25..player.posX.toInt() + 25
-                        val zRange = player.posZ.toInt() - 25..player.posZ.toInt() + 25
-                        roomRotation@
-                        for (te in world.loadedTileEntityList) {
-                            if (te.pos.y == 56 && te is TileEntityChest && te.numPlayersUsing == 0 && te.pos.x in xRange && te.pos.z in zRange) {
-                                if (world.getBlockState(te.pos.down()).block == Blocks.stone && world.getBlockState(te.pos.up(2)).block == Blocks.stained_glass) {
-                                    for (horizontal in EnumFacing.HORIZONTALS) {
-                                        if (world.getBlockState(te.pos.offset(horizontal.opposite, 3).down(2)).block == Blocks.sticky_piston) {
-                                            if (world.getBlockState(te.pos.offset(horizontal, 2)).block == Blocks.stone) {
-                                                chestPos = te.pos
-                                                roomFacing = horizontal
-                                                break@roomRotation
-                                            }
+    init {
+        execute(1000) {
+            if (!DungeonUtils.inDungeons) return@execute
+            val player = Minecraft.getMinecraft().thePlayer ?: return@execute
+            val world = Minecraft.getMinecraft().theWorld ?: return@execute
+            ModCore.scope.launch {
+                prevInWaterRoom = inWaterRoom
+                inWaterRoom = false
+                if (BlockPos.getAllInBox(
+                        BlockPos(player.posX.toInt() - 13, 54, player.posZ.toInt() - 13),
+                        BlockPos(player.posX.toInt() + 13, 54, player.posZ.toInt() + 13))
+                        .any { world.getBlockState(it).block == Blocks.sticky_piston }) {
+                    val xRange = player.posX.toInt() - 25..player.posX.toInt() + 25
+                    val zRange = player.posZ.toInt() - 25..player.posZ.toInt() + 25
+                    roomRotation@
+                    for (te in world.loadedTileEntityList) {
+                        if (te.pos.y == 56 && te is TileEntityChest && te.numPlayersUsing == 0 && te.pos.x in xRange && te.pos.z in zRange) {
+                            if (world.getBlockState(te.pos.down()).block == Blocks.stone && world.getBlockState(te.pos.up(2)).block == Blocks.stained_glass) {
+                                for (horizontal in EnumFacing.HORIZONTALS) {
+                                    if (world.getBlockState(te.pos.offset(horizontal.opposite, 3).down(2)).block == Blocks.sticky_piston) {
+                                        if (world.getBlockState(te.pos.offset(horizontal, 2)).block == Blocks.stone) {
+                                            chestPos = te.pos
+                                            roomFacing = horizontal
+                                            break@roomRotation
                                         }
                                     }
                                 }
                             }
                         }
-
-                        if (chestPos == null) return@launch
-
-                        findWaterRoom()
-
-                    } else {
-                        solutions.clear()
-                        variant = -1
                     }
+
+                    if (chestPos == null) return@launch
+
+                    findWaterRoom()
+
+                } else {
+                    solutions.clear()
+                    variant = -1
                 }
             }
-            ticks = 0
         }
-        ticks++
+
+        onWorldLoad { reset() }
     }
 
     @SubscribeEvent
@@ -124,17 +118,12 @@ object WaterSolver : Module(
 
     @SubscribeEvent
     fun onBlockInteract(event: PlayerInteractEvent) {
-        if (event.action != PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) return
-        if (solutions.isEmpty()) return
+        if (event.action != PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK || solutions.isEmpty()) return
         for (value in LeverBlock.entries) {
-            if (value.leverPos == event.pos) {
-                value.i++
-                if (value == LeverBlock.WATER) {
-                    if (openedWater == -1L) {
-                        openedWater = System.currentTimeMillis()
-                    }
-                }
-            }
+            if (value.leverPos != event.pos) continue
+            value.i++
+            if (value != LeverBlock.WATER || openedWater != -1L) continue
+            openedWater = System.currentTimeMillis()
         }
     }
 
@@ -223,26 +212,20 @@ object WaterSolver : Module(
         }
     }
 
-    @SubscribeEvent
-    fun reset(event: WorldEvent.Load) {
+    fun reset() {
         chestPos = null
         roomFacing = null
         prevInWaterRoom = false
         inWaterRoom = false
         variant = -1
         extendedSlots = ""
-        ticks = 0
         solutions.clear()
         openedWater = -1L
         LeverBlock.entries.forEach { it.i = 0 }
     }
 
-    enum class WoolColor(var dyeColor: EnumDyeColor) {
-        PURPLE(EnumDyeColor.PURPLE),
-        ORANGE(EnumDyeColor.ORANGE),
-        BLUE(EnumDyeColor.BLUE),
-        GREEN(EnumDyeColor.GREEN),
-        RED(EnumDyeColor.RED);
+    enum class WoolColor {
+        PURPLE, ORANGE, BLUE, GREEN, RED;
 
         val isExtended: Boolean
             get() = if (chestPos == null || roomFacing == null) false else Minecraft.getMinecraft().theWorld.getBlockState(
@@ -250,14 +233,7 @@ object WaterSolver : Module(
     }
 
     enum class LeverBlock(var i: Int = 0) {
-        QUARTZ,
-        GOLD,
-        COAL,
-        DIAMOND,
-        EMERALD,
-        CLAY,
-        WATER,
-        NONE;
+        QUARTZ, GOLD, COAL, DIAMOND, EMERALD, CLAY, WATER, NONE;
 
         val leverPos: BlockPos?
             get() {
@@ -272,6 +248,4 @@ object WaterSolver : Module(
                 }
             }
     }
-
-
 }
