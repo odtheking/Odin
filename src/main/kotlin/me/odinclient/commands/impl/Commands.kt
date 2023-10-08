0be74b67e331b3ad0d252f5dbba8dbfe4cb9a3bf
Stivais <Stivaisss@gmail.com>
2023-10-08 20:30:46 +0300

    try not to make sihrt men

--- src/main/kotlin/me/odinclient/commands/impl/Commands.kt
+++ src/main/kotlin/me/odinclient/commands/impl/Commands.kt
@@ -46,7 +46,6 @@ val mainCommand = "od" {
                 if (setRotation(it[0], it[1])) modMessage("Set yaw and pitch to ${it[0]}, ${it[1]}")
                 else modMessage("§cInvalid yaw and or pitch.")
             }
-    }
         }
 
         "yaw" does {
@@ -64,27 +63,45 @@ val mainCommand = "od" {
                 else modMessage("§cInvalid pitch.")
             }
         }
+    }
 
-    "rq" {
+    "rq" does {
         ChatUtils.sendCommand("instancerequeue")
         modMessage("requeing dungeon run")
     }
 
     "help" {
-        modMessage(
-            "List of commands:" +
-                    "\n§7od help §8- §7Shows this message." +
-                    "\n§7od §8- §7Opens the click gui." +
-                    "\n§7od hud §8- §7Opens the hud editor." +
-                    "\n§7od reset (clickgui|hud) §8- §7Resets the click gui or hud positions." +
-                    "\n§7od set (pitch|yaw) # §8- §7Sets your yaw and pitch to #." +
-                    "\n§7od rq §8- §7Requeues your dungeon run." +
-                    "\n§7od f# §8- §7Joins floor #." +
-                    "\n§7od m# §8- §7Joins master floor #." +
-                    "\n§7termsim §8- §7Opens the term simulator."
+        does {
+            modMessage("""
+            List of commands:
+             - /od » §7Main command. Do `/od help this` for more info about this command.
+             - /autosell » §7Used to configure what items are automatically sold with Auto Sell.
+             - /blacklist » §7 Word this to describe what blacklist even does.
+             - /esp » §7Used to configure ESP list.
+             - /waypoint » §7Configure waypoints.
+             - /termsim » §7Simulates terminals so you can practice them.
+            """.trimIndent())
+        }
 
-        )
+        "this" does {
+            modMessage("""
+                hi someone else do this and dont make it shit 
+            """.trimIndent())
+        }
     }
+
+
+    /*"List of commands:" +
+            "\n§7od help §8- §7Shows this message." +
+            "\n§7od §8- §7Opens the click gui." +
+            "\n§7od hud §8- §7Opens the hud editor." +
+            "\n§7od reset (clickgui|hud) §8- §7Resets the click gui or hud positions." +
+            "\n§7od set (pitch|yaw) # §8- §7Sets your yaw and pitch to #." +
+            "\n§7od rq §8- §7Requeues your dungeon run." +
+            "\n§7od f# §8- §7Joins floor #." +
+            "\n§7od m# §8- §7Joins master floor #." +
+            "\n§7termsim §8- §7Opens the term simulator."
+*/
 
     does {
         if (it.isEmpty()) display = ClickGUI
@@ -97,7 +114,7 @@ val mainCommand = "od" {
                 val prefix = if (type == 'm') "master_" else ""
                 ChatUtils.sendCommand("joininstance ${prefix}catacombs_floor_$floor ")
             } else {
-                modMessage("§cInvalid command! Use od help for a list of commands.")
+                modMessage("§cInvalid command! Use `od help this` for a list of commands.")
             }
         }
     }
@@ -106,7 +123,7 @@ val mainCommand = "od" {
 private val numberMap = mapOf(1 to "one", 2 to "two", 3 to "three", 4 to "four", 5 to "five", 6 to "six", 7 to "seven")
 
 /** Function to parse strings and set your yaw and pitch. */
-fun setRotation(yaw: String = "0", pitch: String = "0"): Boolean {
+private fun setRotation(yaw: String = "0", pitch: String = "0"): Boolean {
     yaw.toFloatOrNull()?.let { mc.thePlayer.rotationYaw = it } ?: return false
     pitch.toFloatOrNull()?.let { mc.thePlayer.rotationPitch = it } ?: return false
     return true
