f52b4fe6c6d4a74b85c13d2f954209d4389bd73e
freebonsai <118639445+freebonsai@users.noreply.github.com>
2023-10-08 15:54:42 +0200

    /od simulate command

--- src/main/kotlin/me/odinclient/commands/impl/Commands.kt
+++ src/main/kotlin/me/odinclient/commands/impl/Commands.kt
@@ -3,12 +3,15 @@ package me.odinclient.commands.impl
 import me.odinclient.ModCore.Companion.display
 import me.odinclient.ModCore.Companion.mc
 import me.odinclient.commands.invoke
+import me.odinclient.events.impl.ChatPacketEvent
 import me.odinclient.features.impl.floor7.p3.termsim.StartGui
 import me.odinclient.features.impl.render.ClickGUIModule
 import me.odinclient.ui.clickgui.ClickGUI
 import me.odinclient.ui.hud.EditHUDGui
 import me.odinclient.utils.skyblock.ChatUtils
 import me.odinclient.utils.skyblock.ChatUtils.modMessage
+import net.minecraft.util.ChatComponentText
+import net.minecraftforge.common.MinecraftForge
 
 val termSimCommand = "termsim" {
     does {
@@ -50,24 +53,26 @@ val mainCommand = "od" {
         }
 
         "yaw" does {
-            if (it.size != 1) modMessage("§cMissing yaw!")
-            else {
-                if (setRotation(yaw = it[0])) modMessage("Set yaw to ${it[0]}")
-                else modMessage("§cInvalid yaw.")
-            }
+            if (it.size != 1) return@does modMessage("§cMissing yaw!")
+            if (setRotation(yaw = it[0])) modMessage("Set yaw to ${it[0]}")
+            else modMessage("§cInvalid yaw.")
         }
 
         "pitch" does {
-            if (it.size != 1) modMessage("§cMissing pitch!")
-            else {
-                if (setRotation(pitch = it[0])) modMessage("Set pitchto ${it[0]}")
-                else modMessage("§cInvalid pitch.")
-            }
+            if (it.size != 1) return@does modMessage("§cMissing pitch!")
+            if (setRotation(pitch = it[0])) modMessage("Set pitch to ${it[0]}")
+            else modMessage("§cInvalid pitch.")
         }
 
     "rq" {
         ChatUtils.sendCommand("instancerequeue")
         modMessage("requeing dungeon run")
+    }
+
+    "simulate" does {
+        if (it.isEmpty()) return@does modMessage("§cMissing message!")
+        mc.thePlayer.addChatMessage(ChatComponentText(it.joinToString(" ")))
+        MinecraftForge.EVENT_BUS.post(ChatPacketEvent(it.joinToString(" ")))
     }
 
     does {
@@ -76,25 +81,24 @@ val mainCommand = "od" {
             val arg = it[0]
             if (arg == "help")  modMessage(
                 "List of commands:" +
-                        "\n§7od help §8- §7Shows this message." +
-                        "\n§7od §8- §7Opens the click gui." +
-                        "\n§7od hud §8- §7Opens the hud editor." +
-                        "\n§7od reset (clickgui|hud) §8- §7Resets the click gui or hud positions." +
-                        "\n§7od set (pitch|yaw) # §8- §7Sets your yaw and pitch to #." +
-                        "\n§7od rq §8- §7Requeues your dungeon run." +
-                        "\n§7od f# §8- §7Joins floor #." +
-                        "\n§7od m# §8- §7Joins master floor #." +
-                        "\n§7termsim §8- §7Opens the term simulator." +
-                        "\n§7od esp help §8- §7Configures the esp commands." +
-                        "\n§7od blacklist help §8- §7Configures the blacklist commands." +
-                        "\n§7od autosell help §8- §7Configures the autosell commands."
+                        "\n§7- od help §8- §7Shows this message." +
+                        "\n§7- od §8- §7Opens the click gui." +
+                        "\n§7- od hud §8- §7Opens the hud editor." +
+                        "\n§7- od reset (clickgui|hud) §8- §7Resets the click gui or hud positions." +
+                        "\n§7- od set (pitch|yaw) # §8- §7Sets your yaw and pitch to #." +
+                        "\n§7- od rq §8- §7Requeues your dungeon run." +
+                        "\n§7- od f# §8- §7Joins floor #." +
+                        "\n§7- od m# §8- §7Joins master floor #." +
+                        "\n§7- termsim §8- §7Opens the term simulator." +
+                        "\n§7- od esp help §8- §7Configures the esp commands." +
+                        "\n§7- od blacklist help §8- §7Configures the blacklist commands." +
+                        "\n§7- od autosell help §8- §7Configures the autosell commands."
             )
             else if (arg.first() == 'f' || arg.first() == 'm') {
-                if (arg.length != 2 || !arg[1].isDigit()) return@does modMessage("§cInvalid floorfr!")
+                if (arg.length != 2 || !arg[1].isDigit()) return@does modMessage("§cInvalid floor!")
                 val type = it.first().first()
                 val floor = numberMap[it.first()[1].digitToInt()] ?: return@does modMessage("§cInvalid floor!")
-                val prefix = if (type == 'm') "master_" else ""
-                ChatUtils.sendCommand("joininstance ${prefix}catacombs_floor_$floor ")
+                ChatUtils.sendCommand("joininstance ${if (type == 'm') "master_" else ""}catacombs_floor_$floor ")
             } else {
                 modMessage("§cInvalid command! Use od help for a list of commands.")
             }
