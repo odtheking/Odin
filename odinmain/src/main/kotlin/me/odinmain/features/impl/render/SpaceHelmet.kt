ee58055d1131442e58307df722c1dc151a17edfd
Cezqr <Cezqr1337@gmail.com>
2023-10-29 14:27:11 +0100

    small SpaceHelmet optimization

--- odinmain/src/main/kotlin/me/odinmain/features/impl/render/SpaceHelmet.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/render/SpaceHelmet.kt
@@ -4,7 +4,6 @@ import me.odinmain.features.Category
 import me.odinmain.features.Module
 import me.odinmain.utils.clock.Executor
 import me.odinmain.utils.clock.Executor.Companion.register
-import me.odinmain.utils.skyblock.ChatUtils
 import net.minecraft.init.Blocks
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
@@ -19,14 +18,13 @@ object SpaceHelmet : Module(
 
     init {
         Executor(delay = 250) {
-            if (mc.thePlayer == null) return@Executor
+            if (mc.thePlayer == null || !enabled) return@Executor
 
             val color = values[currentIndex]
             currentIndex = (currentIndex + 1) % values.size
-            ChatUtils.modMessage("$color")
             val item = ItemStack(Item.getItemFromBlock(Blocks.stained_glass), 1, color) . apply {setStackDisplayName("§c§lSpace Helmet") }
 
-            if (enabled) mc.thePlayer?.inventory?.armorInventory?.set(3, item)
+            mc.thePlayer?.inventory?.armorInventory?.set(3, item)
 
         }.register()
     }
