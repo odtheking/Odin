8598c1310db85df133a7595e16b612fa12fa43e0
Cezqr <Cezqr1337@gmail.com>
2023-11-04 13:24:00 +0100

    etherwarphelper accessor

---
+++ odin/src/main/java/me/odin/mixin/accessors/IEntityPlayerSPAccessor.java
@@ -0,0 +1,25 @@
+package me.odin.mixin.accessors;
+
+import net.minecraft.client.entity.EntityPlayerSP;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(EntityPlayerSP.class)
+public interface IEntityPlayerSPAccessor {
+
+    @Accessor("lastReportedPosX")
+    double getLastReportedPosX();
+
+    @Accessor("lastReportedPosY")
+    double getLastReportedPosY();
+
+    @Accessor("lastReportedPosZ")
+    double getLastReportedPosZ();
+
+    @Accessor("lastReportedYaw")
+    float getLastReportedYaw();
+
+    @Accessor("lastReportedPitch")
+    float getLastReportedPitch();
+
+}
