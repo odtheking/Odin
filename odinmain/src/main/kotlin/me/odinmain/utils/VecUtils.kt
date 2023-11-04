e6b4f2b4ebc7700854059f8cacb98913c611bab3
Cezqr <Cezqr1337@gmail.com>
2023-11-04 12:06:56 +0100

    make it go on top of block

--- odinmain/src/main/kotlin/me/odinmain/utils/VecUtils.kt
+++ odinmain/src/main/kotlin/me/odinmain/utils/VecUtils.kt
@@ -168,14 +168,14 @@ object VecUtils {
     /**
      * @param add Will determine the maximum bounds
      */
-    fun BlockPos.toAABB(add: Double = 1.0): AxisAlignedBB {
+    fun BlockPos.toAABB(add: Double = 1.001): AxisAlignedBB {
         return AxisAlignedBB(this.x.toDouble(), this.y.toDouble(), this.z.toDouble(), this.x + add, this.y + add, this.z + add).expand(0.01, 0.01, 0.01)
     }
 
     /**
      * @param add Will determine the maximum bounds
      */
-    fun Vec3.toAABB(add: Double = 1.0): AxisAlignedBB {
+    fun Vec3.toAABB(add: Double = 1.001): AxisAlignedBB {
         return AxisAlignedBB(this.xCoord, this.yCoord, this.zCoord, this.xCoord + add, this.yCoord + add, this.zCoord + add)
     }
 
