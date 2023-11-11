665d3f7141df91e37477fe741c4463aa45fa7e7f
freebonsai <118639445+freebonsai@users.noreply.github.com>
2023-11-11 16:54:17 +0100

    maybe render optimizer better now?

--- odinmain/src/main/kotlin/me/odinmain/features/impl/render/RenderOptimizer.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/render/RenderOptimizer.kt
@@ -6,6 +6,7 @@ import me.odinmain.features.settings.impl.BooleanSetting
 import me.odinmain.utils.skyblock.dungeon.DungeonUtils
 import net.minecraft.entity.item.EntityArmorStand
 import net.minecraft.entity.item.EntityFallingBlock
+import net.minecraft.util.AxisAlignedBB
 import net.minecraftforge.event.entity.EntityJoinWorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -17,6 +18,14 @@ object RenderOptimizer : Module(
 
     private val fallingBlocks: Boolean by BooleanSetting(name = "Remove Falling Blocks", default = true)
     private val p5Mobs: Boolean by BooleanSetting(name = "Remove P5 Armor Stands", default = true)
+    private val p5Boxes = listOf(
+        AxisAlignedBB(47.0, 4.0, 70.0, 61.0, 7.0, 84.0),
+        AxisAlignedBB(18.0, 4.0, 50.0, 30.0, 22.0, 64.0),
+        AxisAlignedBB(18.0, 4.0, 86.0, 30.0, 22.0, 99.0),
+        AxisAlignedBB(42.0, 4.0, 134.0, 70.0, 22.0, 119.0),
+        AxisAlignedBB(93.0, 4.0, 85.0, 79.0, 24.0, 100.0),
+        AxisAlignedBB(95.0, 4.0, 48.0, 80.0, 24.0, 64.0),
+    )
 
     @SubscribeEvent
     fun onFallingBlock(event: EntityJoinWorldEvent) {
@@ -24,9 +33,7 @@ object RenderOptimizer : Module(
             event.entity is EntityArmorStand &&
             p5Mobs &&
             DungeonUtils.getPhase() == 5 &&
-            event.entity.posY < 15 && // don't kill dragon tags
-            event.entity.posX !in 47.0..61.0 && event.entity.posZ !in 70.0..84.0 && // chest positions
-            !event.entity.name.contains("relic", true)
+            p5Boxes.none { it.isVecInside(event.entity.positionVector) }
         )
             event.entity.setDead()
 
