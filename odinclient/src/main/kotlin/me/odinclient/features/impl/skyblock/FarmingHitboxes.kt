ddb3e7108f9c4e94bd4ba79d5d08ac8ea18f6186
Cezqr <Cezqr1337@gmail.com>
2023-11-19 23:58:54 +0100

    farming hitboxes

--- odinclient/src/main/kotlin/me/odinclient/features/impl/skyblock/FarmingHitboxes.kt
+++ odinclient/src/main/kotlin/me/odinclient/features/impl/skyblock/FarmingHitboxes.kt
@@ -2,9 +2,7 @@ package me.odinclient.features.impl.skyblock
 
 import me.odinmain.features.Category
 import me.odinmain.features.Module
-import me.odinmain.features.settings.impl.BooleanSetting
 import net.minecraft.block.Block
-import net.minecraft.block.BlockCocoa
 
 object FarmingHitboxes : Module(
     name = "Farming Hitboxes",
@@ -13,16 +11,8 @@ object FarmingHitboxes : Module(
     tag = TagType.NEW
 ) {
 
-    val mushroom: Boolean by BooleanSetting(name = "Mushroom", default = true)
-    private val cocoa: Boolean by BooleanSetting(name = "Cocoa", default = true)
-
-    fun setBlockBoundsMixin(block: Block): Boolean
-    {
-        if (this.enabled)
-        {
-            if (cocoa && block is BlockCocoa) { block.setBlockBounds(0f, 0f, 0f, 1f, 1f, 1f); return true }
-        }
-        return false
+    fun setFullBlock(block: Block) {
+        block.setBlockBounds(0f, 0f, 0f, 1f, 1f, 1f)
     }
 
 }
