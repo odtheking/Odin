5f00b18564e744e4c24e789181a0fc5bf9d50667
Cezqr <Cezqr1337@gmail.com>
2023-12-07 00:07:31 +0100

    Maybe fix crystal triggerbot

--- odinclient/src/main/kotlin/me/odinclient/features/impl/skyblock/Triggerbot.kt
+++ odinclient/src/main/kotlin/me/odinclient/features/impl/skyblock/Triggerbot.kt
@@ -68,7 +68,7 @@ object Triggerbot : Module(
     @SubscribeEvent
     fun onTick(event: TickEvent.ClientTickEvent) {
         if (!DungeonUtils.inBoss || DungeonUtils.getPhase() != 1 || !clickClock.hasTimePassed() || mc.objectMouseOver == null) return
-        if (take && mc.objectMouseOver.entityHit is EntityEnderCrystal || (place && mc.objectMouseOver.entityHit?.name?.noControlCodes == "Energy Crystal Missing" && mc.thePlayer.heldItem.displayName.noControlCodes == "Energy Crystal")) {
+        if ((take && mc.objectMouseOver.entityHit is EntityEnderCrystal) || (place && mc.objectMouseOver.entityHit?.name?.noControlCodes == "Energy Crystal Missing" && mc.thePlayer.heldItem.displayName.noControlCodes == "Energy Crystal")) {
             PlayerUtils.rightClick()
             clickClock.update()
         }
