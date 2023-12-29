e14ede8b48e895fd4812764e1b27375f60a4a3ac
freebonsai <118639445+freebonsai@users.noreply.github.com>
2023-12-29 21:41:45 +0100

    less schizo maybe? didnt test.

--- odinmain/src/main/kotlin/me/odinmain/features/impl/dungeon/LeapMenu.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/dungeon/LeapMenu.kt
@@ -3,16 +3,12 @@ package me.odinmain.features.impl.dungeon
 import me.odinmain.events.impl.DrawGuiScreenEvent
 import me.odinmain.events.impl.GuiClickEvent
 import me.odinmain.events.impl.GuiLoadedEvent
-import me.odinmain.events.impl.ServerTickEvent
 import me.odinmain.features.Category
 import me.odinmain.features.Module
 import me.odinmain.features.settings.impl.DualSetting
-import me.odinmain.ui.clickgui.util.ColorUtil.withAlpha
 import me.odinmain.utils.name
 import me.odinmain.utils.noControlCodes
 import me.odinmain.utils.render.Color
-import me.odinmain.utils.skyblock.PlayerUtils
-import me.odinmain.utils.skyblock.dungeon.DungeonUtils
 import me.odinmain.utils.skyblock.dungeon.DungeonUtils.Classes
 import me.odinmain.utils.skyblock.dungeon.DungeonUtils.DungeonPlayer
 import me.odinmain.utils.skyblock.modMessage
@@ -24,7 +20,6 @@ import net.minecraft.inventory.ContainerChest
 import net.minecraft.item.ItemSkull
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import org.lwjgl.input.Mouse
 
 object LeapMenu : Module(
     name = "Leap Menu",
@@ -33,11 +28,10 @@ object LeapMenu : Module(
 ) {
     private val type: Boolean by DualSetting("Sorting", "Class", "Name", default = true)
 
-    private var leapTeammates1 : Array<DungeonPlayer?>? = mutableListOf(
+    private val EMPTY = DungeonPlayer("Empty", Classes.Archer, ResourceLocation("textures/entity/steve.png"))
+    private var leapTeammates1: Array<DungeonPlayer>? = mutableListOf(
         DungeonPlayer("Stiviaisd", Classes.Archer, ResourceLocation("textures/entity/steve.png")),
-
-
-        ).toTypedArray()
+    ).toTypedArray()
 
     private val leapTeammates =
         leapTeammates1?.let { fillPlayerList(it) }
@@ -56,15 +50,16 @@ object LeapMenu : Module(
         modMessage("Teleporting to ${playerToLeap?.name} (${playerToLeap?.clazz?.name}) in index $index.")
 
         mc.playerController.windowClick(event.gui.inventorySlots.windowId, 11 + index, 1, 2, mc.thePlayer)
+        event.isCanceled = true
     }
 
-    private fun fillPlayerList(players: Array<DungeonPlayer?>): Array<DungeonPlayer?> {
-        val sortedPlayers = players.sortedBy { it?.clazz?.prio }
-        val result = arrayOfNulls<DungeonPlayer?>(4)
+    private fun fillPlayerList(players: Array<DungeonPlayer>): Array<DungeonPlayer> {
+        val sortedPlayers = players.sortedBy { it.clazz.prio }
+        val result = Array(4) { EMPTY }
         val secondRound = mutableListOf<DungeonPlayer>()
 
         for (player in sortedPlayers) {
-            if (result[player?.clazz?.defaultQuandrant!!] == null) {
+            if (result[player.clazz.defaultQuandrant] == EMPTY) {
                 result[player.clazz.defaultQuandrant] = player
                 println("Added $player to result.")
             } else {
@@ -79,14 +74,14 @@ object LeapMenu : Module(
         if (secondRound.isEmpty()) return result
 
         result.forEachIndexed { index, player ->
-            if (result[index] == null) {
+            if (result[index] == EMPTY) {
                 println(index)
                 result[index] = secondRound.removeAt(0)
                 if (secondRound.isEmpty()) return result
                 println("Added $player to result.")
             }
         }
-        println("Result: ${result.joinToString(", ") { it?.name ?: "null" }}")
+        println("Result: ${result.joinToString(", ") { it.name }}")
         return result
     }
 
@@ -118,7 +113,7 @@ object LeapMenu : Module(
 
         val sr = ScaledResolution(mc)
         leapTeammates?.forEachIndexed { index, it ->
-            if (it == null) return@forEachIndexed
+            if (it == EMPTY) return@forEachIndexed
             GlStateManager.pushMatrix()
             GlStateManager.enableAlpha()
             GlStateManager.scale(6.0 / sr.scaleFactor,  6.0 / sr.scaleFactor, 1.0)
@@ -130,9 +125,9 @@ object LeapMenu : Module(
 
             GlStateManager.color(255f, 255f, 255f, 255f)
             Gui.drawScaledCustomSizeModalRect(-10, -10, 8f, 8f, 8, 8, 40, 40, 64f, 64f)
-            if (it != null) {
-                mc.fontRendererObj.drawString(it.name, 35, 5, it.clazz.color.rgba)
-            }
+
+            mc.fontRendererObj.drawString(it.name, 35, 5, it.clazz.color.rgba)
+
 
             GlStateManager.disableAlpha()
             GlStateManager.popMatrix()
@@ -141,22 +136,11 @@ object LeapMenu : Module(
 
     }
     private fun getQuadrant(mouseX: Int, mouseY: Int): Int {
-        val screenWidth = mc.displayWidth / 2
-        val screenHeight = mc.displayHeight / 2
-
-        val centerX = screenWidth / 2
-        val centerY = screenHeight / 2
+        val screenY = mc.displayHeight / 4
 
         return when {
-            (mouseX < centerX && mouseY < centerY) -> 1
-            (mouseX >= centerX && mouseY < centerY) -> 2
-            (mouseX < centerX && mouseY >= centerY) -> 3
-            else -> 4
+            mouseX >= mc.displayWidth / 4 -> if (mouseY >= screenY) 4 else 2
+            else -> if (mouseY >= screenY) 3 else 1
         }
     }
-
-
-
-
-
 }
