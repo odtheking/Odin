b4a1001af57b8a4e7b0e47fdf39564e7748c37fc
Cezqr <Cezqr1337@gmail.com>
2023-12-30 21:28:54 +0100

    NoBreakReset crash fix

--- odinclient/src/main/java/me/odinclient/mixin/mixins/MixinPlayerControllerMP.java
+++ odinclient/src/main/java/me/odinclient/mixin/mixins/MixinPlayerControllerMP.java
@@ -1,12 +1,10 @@
 package me.odinclient.mixin.mixins;
 
 import me.odinclient.features.impl.skyblock.NoBreakReset;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.PlayerControllerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import org.spongepowered.asm.mixin.Debug;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,22 +17,11 @@ public class MixinPlayerControllerMP {
 
     @Shadow private ItemStack currentItemHittingBlock;
 
-    @Shadow @Final private Minecraft mc;
-
     @Shadow private BlockPos currentBlock;
 
     @Inject(method = "isHittingPosition", at = @At("RETURN"), cancellable = true)
     private void onIsHittingPosition(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        if (NoBreakReset.INSTANCE.getEnabled()) {
-            ItemStack itemStack = this.mc.thePlayer.getHeldItem();
-            String lore = itemStack.getTagCompound().getCompoundTag("display").getTagList("Lore", 8).toString();
-            if (lore.contains("GAUNTLET") || lore.contains("DRILL") || lore.contains("PICKAXE")) {
-                cir.setReturnValue(pos.equals(this.currentBlock) && (itemStack.getItem() == this.currentItemHittingBlock.getItem()));
-            }
-        }
-        else {
-            cir.setReturnValue(cir.getReturnValue());
-        }
+        NoBreakReset.INSTANCE.isHittingPositionHook(pos, cir, currentItemHittingBlock, currentBlock);
     }
 
 }
--- odinclient/src/main/kotlin/me/odinclient/features/impl/skyblock/NoBreakReset.kt
+++ odinclient/src/main/kotlin/me/odinclient/features/impl/skyblock/NoBreakReset.kt
@@ -2,6 +2,9 @@ package me.odinclient.features.impl.skyblock
 
 import me.odinmain.features.Category
 import me.odinmain.features.Module
+import net.minecraft.item.ItemStack
+import net.minecraft.util.BlockPos
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
 
 /**
  * @see me.odinclient.mixin.mixins.MixinPlayerControllerMP
@@ -14,6 +17,18 @@ object NoBreakReset : Module(
 
 ) {
 
-
+    fun isHittingPositionHook(pos: BlockPos, cir: CallbackInfoReturnable<Boolean>, currentItemHittingBlock: ItemStack, currentBlock: BlockPos) {
+        if (enabled) {
+            val itemStack = mc.thePlayer.heldItem
+            val lore = itemStack?.tagCompound?.getCompoundTag("display")?.getTagList("Lore", 8)?.toString()
+            if (lore != null) {
+                if (lore.contains("GAUNTLET") || lore.contains("DRILL") || lore.contains("PICKAXE")) {
+                    cir.setReturnValue(pos == currentBlock && (itemStack.item === currentItemHittingBlock.item))
+                }
+            }
+        } else {
+            cir.setReturnValue(cir.returnValue)
+        }
+    }
 
 }
