6824b1968c9264dda2cc6104e24b6c506981695c
freebonsai <118639445+freebonsai@users.noreply.github.com>
2024-03-02 17:01:09 +0100

    GhostPick now allows a keybind for axe too.

--- odinclient/src/main/kotlin/me/odinclient/features/impl/dungeon/GhostPick.kt
+++ odinclient/src/main/kotlin/me/odinclient/features/impl/dungeon/GhostPick.kt
@@ -2,11 +2,12 @@ package me.odinclient.features.impl.dungeon
 
 import me.odinmain.features.Category
 import me.odinmain.features.Module
-import me.odinmain.features.settings.impl.NumberSetting
+import me.odinmain.features.settings.impl.*
 import me.odinmain.utils.runIn
 import net.minecraft.enchantment.Enchantment
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import org.lwjgl.input.Keyboard
 
 object GhostPick : Module(
     "Ghost Pickaxe",
@@ -16,11 +17,18 @@ object GhostPick : Module(
     private val slot: Int by NumberSetting("Ghost pick slot", 1, 1.0, 9.0, 1.0)
     private val level: Int by NumberSetting("Efficiency level", 10, 1.0, 100.0, 1.0)
     private val delay: Int by NumberSetting("Delay to Create (ms)", 0, 0, 1000)
+    private val axeKeybind: Keybinding by KeybindSetting("Axe Keybind", Keyboard.KEY_NONE, description = "Keybind to create an axe.").onPress {
+        giveItem(279)
+    }
 
     override fun onKeybind() {
+        giveItem(278)
+    }
+
+    private fun giveItem(id: Int) {
         if (mc.thePlayer == null || mc.currentScreen != null) return
         runIn(delay / 50) {
-            val item = ItemStack(Item.getItemById(278), 1).apply {
+            val item = ItemStack(Item.getItemById(id), 1).apply {
                 addEnchantment(Enchantment.getEnchantmentById(32), level)
                 tagCompound?.setBoolean("Unbreakable", true)
             }
