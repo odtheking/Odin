bdc95c9ab7b4a9d90ab1e1f1a48e690528f1cdd9
SubAt0mic <pokemonjunkbox@gmail.com>
2024-04-03 14:10:25 -0400

    Added a display for reaper armor enrage

---
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/EnrageDisplay.kt
@@ -0,0 +1,56 @@
+package me.odinmain.features.impl.skyblock
+
+import me.odinmain.events.impl.ServerTickEvent
+import me.odinmain.features.Category
+import me.odinmain.features.Module
+import me.odinmain.features.settings.impl.BooleanSetting
+import me.odinmain.features.settings.impl.HudSetting
+import me.odinmain.font.OdinFont
+import me.odinmain.ui.hud.HudElement
+import me.odinmain.utils.render.Color
+import me.odinmain.utils.render.getTextWidth
+import me.odinmain.utils.render.text
+import me.odinmain.utils.skyblock.modMessage
+import net.minecraftforge.client.event.sound.PlaySoundEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+object EnrageDisplay : Module (
+    name = "Enrage Display",
+    description = "Timer for cooldown of reaper armor enrage",
+    category = Category.SKYBLOCK
+) {
+    private val hud: HudElement by HudSetting("Enrage Timer Hud", 10f, 10f, 1f, true) {
+        if (it) {
+            text("§4Enrage§f: 119t", 1f, 9f, Color.RED, 12f, OdinFont.REGULAR, shadow = true)
+            getTextWidth("Enrage: 119t", 12f) + 2f to 16f
+        } else {
+            if (enrageTime.time >= 60) {
+                text("§4Enrage§f: ${enrageTime.time}t", 1f, 9f, Color.WHITE, 12f, OdinFont.REGULAR, shadow = true)
+            }
+            if (enrageTime.time in 30..60) {
+                text("§4Enrage§f: §e${enrageTime.time}t", 1f, 9f, Color.YELLOW, 12f, OdinFont.REGULAR, shadow = true)
+            }
+            if (enrageTime.time in 0..30) {
+                text("§4Enrage§f: §4${enrageTime.time}t", 1f, 9f, Color.RED, 12f, OdinFont.REGULAR, shadow = true)
+            }
+
+
+            getTextWidth("Enrage: 119t", 12f) + 2f to 12f
+        }
+    }
+    data class Timer(var time: Int)
+    private var enrageTime = Timer(0)
+
+    @SubscribeEvent
+    fun onSound(event:PlaySoundEvent) {
+        if (event.name != "mob.zombie.remedy") return
+        if (event.sound.pitch != 1.0f) return
+        if (event.sound.volume != .5f) return
+        enrageTime = Timer(120)
+    }
+    @SubscribeEvent
+    fun onServerTick(event: ServerTickEvent) {
+        enrageTime.time--
+    }
+
+}
