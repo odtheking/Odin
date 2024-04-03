870648e8ce6d432701f50b9a48e13b76a188b8f9
SubAt0mic <pokemonjunkbox@gmail.com>
2024-04-03 15:12:34 -0400

    made it detect the packet

--- odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/EnrageDisplay.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/EnrageDisplay.kt
@@ -3,7 +3,6 @@ package me.odinmain.features.impl.skyblock
 import me.odinmain.events.impl.ServerTickEvent
 import me.odinmain.features.Category
 import me.odinmain.features.Module
-import me.odinmain.features.settings.impl.BooleanSetting
 import me.odinmain.features.settings.impl.HudSetting
 import me.odinmain.font.OdinFont
 import me.odinmain.ui.hud.HudElement
@@ -12,7 +11,7 @@ import me.odinmain.utils.render.getTextWidth
 import me.odinmain.utils.render.text
 import me.odinmain.utils.skyblock.itemID
 import me.odinmain.utils.skyblock.modMessage
-import net.minecraftforge.client.event.sound.PlaySoundEvent
+import net.minecraft.network.play.server.S29PacketSoundEffect
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 object EnrageDisplay : Module (
@@ -22,17 +21,17 @@ object EnrageDisplay : Module (
 ) {
     private val hud: HudElement by HudSetting("Enrage Timer Hud", 10f, 10f, 1f, true) {
         if (it) {
-            text("§4Enrage§f: 119t", 1f, 9f, Color.RED, 12f, OdinFont.REGULAR, shadow = true)
+            text("§4Enrage: §a119t", 1f, 9f, Color.RED, 12f, OdinFont.REGULAR, shadow = true)
             getTextWidth("Enrage: 119t", 12f) + 2f to 16f
         } else {
             if (enrageTime.time >= 60) {
-                text("§4Enrage§f: ${enrageTime.time}t", 1f, 9f, Color.WHITE, 12f, OdinFont.REGULAR, shadow = true)
+                text("§4Enrage: §a${enrageTime.time}t", 1f, 9f, Color.WHITE, 12f, OdinFont.REGULAR, shadow = true)
             }
             if (enrageTime.time in 30..60) {
-                text("§4Enrage§f: §e${enrageTime.time}t", 1f, 9f, Color.YELLOW, 12f, OdinFont.REGULAR, shadow = true)
+                text("§4Enrage: §e${enrageTime.time}t", 1f, 9f, Color.WHITE, 12f, OdinFont.REGULAR, shadow = true)
             }
             if (enrageTime.time in 0..30) {
-                text("§4Enrage§f: §4${enrageTime.time}t", 1f, 9f, Color.RED, 12f, OdinFont.REGULAR, shadow = true)
+                text("§4Enrage: §c${enrageTime.time}t", 1f, 9f, Color.WHITE, 12f, OdinFont.REGULAR, shadow = true)
             }
 
 
@@ -42,11 +41,18 @@ object EnrageDisplay : Module (
     data class Timer(var time: Int)
     private var enrageTime = Timer(0)
 
-    @SubscribeEvent
-    fun onSound(event:PlaySoundEvent) {
-        if (event.name != "mob.zombie.remedy" || event.sound.pitch != 1.0f || event.sound.volume != .5f || mc.thePlayer?.getCurrentArmor(0)?.itemID != "REAPER_BOOTS" || mc.thePlayer?.getCurrentArmor(1)?.itemID != "REAPER_LEGGINGS" || mc.thePlayer?.getCurrentArmor(2)?.itemID != "REAPER_CHESTPLATE") return
-        enrageTime = Timer(120)
+    init {
+        onPacket(S29PacketSoundEffect::class.java) {
+            handleSoundPacket(it)
+        }
     }
+
+    private fun handleSoundPacket(it: S29PacketSoundEffect) {
+        if (it.soundName != "mob.zombie.remedy" || it.pitch != 1.0f || it.volume != 0.5f) return
+        enrageTime = Timer(120)
+        modMessage("lol")
+    }
+
     @SubscribeEvent
     fun onServerTick(event: ServerTickEvent) {
         enrageTime.time--
