a551969ef4aa283eba02415cf9f0899652bc143e
SubAt0mic <pokemonjunkbox@gmail.com>
2024-04-14 13:17:35 -0400

    ok cheaterererer works

--- odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/HidePlayers.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/HidePlayers.kt
@@ -8,6 +8,7 @@ import me.odinmain.features.settings.impl.BooleanSetting
 import me.odinmain.features.settings.impl.NumberSetting
 import me.odinmain.utils.ServerUtils.getPing
 import net.minecraftforge.client.event.RenderPlayerEvent
+import net.minecraftforge.event.entity.living.LivingEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 object HidePlayers : Module(
@@ -17,12 +18,24 @@ object HidePlayers : Module(
 ) {
     private val hideAll: Boolean by BooleanSetting("Hide all", default = false, false, "Hides all players, regardless of distance")
     private val distance: Double by NumberSetting("distance", 3.0, 0.0, 32.0, .5, false, "The number of blocks away to hide players.").withDependency { !hideAll }
+    private val clickThrough: Boolean by BooleanSetting("Click Through", default = false, false, "Allows clicking through players.").withDependency { !onLegitVersion }
 
     @SubscribeEvent
     fun onRenderEntity(event: RenderPlayerEvent.Pre) {
-        if (event.entity.getPing() != 1) return
+        if (event.entity.getPing() != 1 || !clickThrough) return
         if (hideAll && event.entity != mc.thePlayer) { event.isCanceled = true }
         val distanceTo = event.entity.getDistanceToEntity(mc.thePlayer)
-        if (distanceTo <= distance && !hideAll && event.entity != mc.thePlayer) { event.isCanceled = true }
+        if (distanceTo <= distance && event.entity != mc.thePlayer || hideAll && event.entity != mc.thePlayer ) { event.isCanceled = true }
     }
+
+    @SubscribeEvent
+    fun onPosUpdate(event: LivingEvent.LivingUpdateEvent) {
+        if (event.entity.getPing() != 1 || !clickThrough) return
+        val distanceTo = event.entity.getDistanceToEntity(mc.thePlayer)
+        if (distanceTo <= distance && event.entity != mc.thePlayer || hideAll && event.entity != mc.thePlayer ) {
+            event.entity.posX = 9999999.0
+            event.isCanceled = true
+        }
+    }
+
 }
