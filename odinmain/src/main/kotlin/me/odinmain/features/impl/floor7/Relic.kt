df33636cb3b02b3ded5bb9ceebc901f964b5ef67
SubAt0mic <pokemonjunkbox@gmail.com>
2024-04-16 19:08:49 -0400

    pbs did, in fact, not work. should now.

--- odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/Relic.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/Relic.kt
@@ -52,10 +52,11 @@ object Relic {
         if (!block.equalsOneOf(Blocks.cauldron, Blocks.anvil) || !currentRelic.equalsOneOf("GREEN_KING_RELIC", "PURPLE_KING_RELIC", "BLUE_KING_RELIC", "ORANGE_KING_RELIC", "RED_KING_RELIC")) return
         val relic = Relics.entries.find { it.id == currentRelic } ?: return modMessage("Relic not found")
         val hasPassed = (System.currentTimeMillis() - timer) / 1000.0
-        if (hasPassed < relic.pbTime.value) relic.pbTime.value = hasPassed
 
         if (relicAnnounceTime) modMessage("${relic.colorCode}${relic.name}§f took ${hasPassed}s ${if (hasPassed < relic.pbTime.value) "(§dNew PB)" else ""}",
             chatStyle = createClickStyle(ClickEvent.Action.SUGGEST_COMMAND, Relics.entries.joinToString { "${it.colorCode}${it.name}§f ${it.pbTime.value.round(3)}" }))
+
+        if (hasPassed < relic.pbTime.value) relic.pbTime.value = hasPassed
         timer = 0L
         Config.save()
     }
