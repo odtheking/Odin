35e807c9a2d36441c167b7eb449729e07852fd67
SubAt0mic <pokemonjunkbox@gmail.com>
2024-04-17 17:07:47 -0400

    cooldown working + range toggle

--- odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/GyroWand.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/GyroWand.kt
@@ -3,6 +3,7 @@ package me.odinmain.features.impl.skyblock
 import me.odinmain.events.impl.ClickEvent
 import me.odinmain.features.Category
 import me.odinmain.features.Module
+import me.odinmain.features.settings.Setting.Companion.withDependency
 import me.odinmain.features.settings.impl.BooleanSetting
 import me.odinmain.features.settings.impl.ColorSetting
 import me.odinmain.features.settings.impl.NumberSetting
@@ -12,6 +13,7 @@ import me.odinmain.utils.render.Color
 import me.odinmain.utils.render.Renderer
 import me.odinmain.utils.skyblock.heldItem
 import me.odinmain.utils.skyblock.itemID
+import me.odinmain.utils.skyblock.modMessage
 import net.minecraft.util.Vec3
 import net.minecraftforge.client.event.RenderWorldLastEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -21,18 +23,19 @@ object GyroWand : Module(
     description = "Helpful features for the Gyrokinetic Wand",
     category = Category.SKYBLOCK
 ) {
-    private val color: Color by ColorSetting("Color", Color.MAGENTA.withAlpha(0.5f), allowAlpha = true)
-    private val thickness: Float by NumberSetting("Thickness", 0.4f, 0, 3, 0.05)
-    private val steps: Int by NumberSetting("Smoothness", 40, 20, 80, 1)
-    private val showCooldown: Boolean by BooleanSetting("Show Cooldown", true, description = "Shows the cooldown of the Gyrokinetic Wand.")
-    private val cooldownColor: Color by ColorSetting("Cooldown Color", Color.RED, allowAlpha = true)
-    private val blockWrongClicks: Boolean by BooleanSetting("Block aligning", true, description = "Blocks using the right click ability on Gyrokinetic Wand")
+    private val gyroRange: Boolean by BooleanSetting("Gyro Range", true, description = "Renders a helpful circle to show the range of the Gyrokinetic Wand.")
+    private val color: Color by ColorSetting("Color", Color.MAGENTA.withAlpha(0.5f), allowAlpha = true).withDependency { gyroRange }
+    private val thickness: Float by NumberSetting("Thickness", 0.4f, 0, 3, 0.05).withDependency { gyroRange }
+    private val steps: Int by NumberSetting("Smoothness", 40, 20, 80, 1).withDependency { gyroRange }
+    private val showCooldown: Boolean by BooleanSetting("Show Cooldown", true, description = "Shows the cooldown of the Gyrokinetic Wand.").withDependency { gyroRange }
+    private val cooldownColor: Color by ColorSetting("Cooldown Color", Color.RED, allowAlpha = true).withDependency { showCooldown && gyroRange}
+    private val blockWrongClicks: Boolean by BooleanSetting("Block aligning", true, description = "Blocks using the right click ability on Gyrokinetic Wand.")
 
     private val gyroCooldown = Clock(30_000)
 
     @SubscribeEvent
     fun onRenderWorld(event: RenderWorldLastEvent) {
-        if (heldItem?.itemID != "GYROKINETIC_WAND") return
+        if (heldItem?.itemID != "GYROKINETIC_WAND" || !gyroRange) return
         val pos = mc.thePlayer.rayTrace(25.0, event.partialTicks)?.blockPos ?: return
         val block = mc.theWorld?.getBlockState(pos)?.block ?: return
         if (block.isAir(mc.theWorld, pos)) return
@@ -49,11 +52,13 @@ object GyroWand : Module(
 
     @SubscribeEvent
     fun onRightClick(event: ClickEvent.RightClickEvent) {
-        if (heldItem?.itemID == "GYROKINETIC_WAND" && blockWrongClicks) event.isCanceled = true
+        if (heldItem?.itemID != "GYROKINETIC_WAND" || !blockWrongClicks) return
+        modMessage(gyroCooldown.hasTimePassed())
+        event.isCanceled = true
     }
 
     init {
-        onMessage(Regex("^-\\d+ Mana \\(Gravity Storm\\)")) {
+        onMessage(Regex("-\\d+ Mana \\(Gravity Storm\\)")) {
             gyroCooldown.update()
         }
     }
