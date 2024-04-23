2487c05bc54c00d40997c525be91ed56cb8d0215
odtheking <ohad.dotan@gmail.com>
2024-04-23 23:30:47 +0300

    Bug fixes

--- odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/DragonCheck.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/DragonCheck.kt
@@ -9,6 +9,7 @@ import me.odinmain.features.impl.floor7.WitherDragons.sendTime
 import me.odinmain.features.impl.skyblock.ArrowHit.onDragonSpawn
 import me.odinmain.features.impl.skyblock.ArrowHit.resetOnDragons
 import me.odinmain.utils.equalsOneOf
+import me.odinmain.utils.isVecInXZ
 import me.odinmain.utils.skyblock.modMessage
 import net.minecraft.entity.boss.EntityDragon
 import net.minecraft.entity.item.EntityArmorStand
@@ -25,7 +26,7 @@ object DragonCheck {
 
     fun dragonJoinWorld(event: EntityJoinWorldEvent) {
         if (event.entity !is EntityDragon) return
-        val dragon = WitherDragonsEnum.entries.find { event.entity.positionVector.dragonCheck(it.spawnPos) } ?: return
+        val dragon = WitherDragonsEnum.entries.find { isVecInXZ(event.entity.positionVector, it.boxesDimensions) } ?: return
 
         dragon.spawning = false
         dragon.particleSpawnTime = 0L
