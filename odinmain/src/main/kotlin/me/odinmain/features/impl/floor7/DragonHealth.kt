34edd279daff9fb0316ea96c3df1fdf7407281af
SubAt0mic <pokemonjunkbox@gmail.com>
2024-04-24 16:38:49 -0400

    (probably) made drag health display health greater than 1b

--- odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/DragonHealth.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/DragonHealth.kt
@@ -10,10 +10,11 @@ import net.minecraftforge.client.event.RenderLivingEvent
 object DragonHealth{
     fun renderHP(event: RenderLivingEvent.Post<*>) {
         if (event.entity !is EntityDragon || event.entity.health <= 0) return
-        Renderer.drawStringInWorld(colorHealth(event.entity.health.toInt()), event.entity.renderVec.addVec(y = 1.5), Color.WHITE, depth = false, scale = 0.2f, shadow = true)
+
+        Renderer.drawStringInWorld(colorHealth(event.entity.health), event.entity.renderVec.addVec(y = 1.5), Color.WHITE, depth = false, scale = 0.2f, shadow = true)
     }
 
-    private fun colorHealth(health: Int): String {
+    private fun colorHealth(health: Float): String {
         return when {
             health >= 0.75 -> "§a${formatHealth(health)}"
             health >= 0.5 -> "§e${formatHealth(health)}"
@@ -22,11 +23,11 @@ object DragonHealth{
         }
     }
 
-    private fun formatHealth(health: Int): String {
+    private fun formatHealth(health: Float): String {
         return when {
-            health >= 1_000_000_000 -> "${(health / 1_000_000_000)}b"
-            health >= 1_000_000 -> "${(health / 1_000_000)}m"
-            health >= 1_000 -> "${(health / 1_000)}k"
+            health >= 1_000_000_000 -> "${String.format("%.2f", health / 1_000_000_000)}b"
+            health >= 1_000_000 -> "${(health.toInt() / 1_000_000)}m"
+            health >= 1_000 -> "${(health.toInt() / 1_000)}k"
             else -> "$health"
         }
     }
