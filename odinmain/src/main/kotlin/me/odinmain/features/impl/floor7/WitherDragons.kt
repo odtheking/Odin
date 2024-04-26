c72381055ba1c95c20acc2f71f13b43ff9d0e47b
SubAt0mic <pokemonjunkbox@gmail.com>
2024-04-26 10:42:40 -0400

    should now display arrow count even if drag death isnt detected.

--- odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/WitherDragons.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/WitherDragons.kt
@@ -180,22 +180,18 @@ object WitherDragons : Module(
         if (::priorityDragon.isInitialized && dragon == priorityDragon) {
             if (sendArrowHit && System.currentTimeMillis() - dragon.spawnedTime < dragon.skipKillTime) {
                 modMessage("§fYou have hit §6$arrowsHit §farrows on §${priorityDragon.colorCode}${priorityDragon.name}")
-                resetArrows()
+                arrowsHit = 0
             }
         }
     }
 
-    private fun resetArrows() {
-        this.arrowsHit = 0
-    }
-
     fun arrowSpawn(dragon: WitherDragonsEnum) {
-        if (::priorityDragon.isInitialized) {
-            resetArrows()
+        if (::priorityDragon.isInitialized && dragon == priorityDragon) {
+            arrowsHit = 0
             Timer().schedule(dragon.skipKillTime) {
-                if (dragon.entity?.isEntityAlive == true && dragon == priorityDragon) {
-                    modMessage("§fYou hit §6${arrowsHit} §fon §${dragon.colorCode}${dragon.name} §fin §c${String.format("%.2f", dragon.skipKillTime.toFloat()/1000)} §fSeconds.")
-                    resetArrows()
+                if (dragon.entity?.isEntityAlive == true || arrowsHit > 0) {
+                    modMessage("§fYou hit §6${arrowsHit} §fon §${dragon.colorCode}${dragon.name}${if (dragon.entity?.isEntityAlive == true) "§fin §c${String.format("%.2f", dragon.skipKillTime.toFloat()/1000)} §fSeconds." else "."}")
+                    arrowsHit = 0
                 }
             }
         }
