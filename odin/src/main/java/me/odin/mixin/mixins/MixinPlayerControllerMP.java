93c43f19669c88017f405b99fb36ba4b4ad6537a
freebonsai <118639445+freebonsai@users.noreply.github.com>
2024-04-29 19:12:22 +0200

    doesnt really work i need to delete project files tho

--- odin/src/main/java/me/odin/mixin/mixins/MixinPlayerControllerMP.java
+++ odin/src/main/java/me/odin/mixin/mixins/MixinPlayerControllerMP.java
@@ -1,37 +1,19 @@
-package me.odinclient.mixin.mixins;
+package me.odin.mixin.mixins;
 
-import me.odinclient.features.impl.skyblock.NoBreakReset;
 import me.odinmain.events.impl.GuiEvent;
 import net.minecraft.client.multiplayer.PlayerControllerMP;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockPos;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Debug(export = true)
 @Mixin(PlayerControllerMP.class)
 public class MixinPlayerControllerMP {
-
-    @Shadow private ItemStack currentItemHittingBlock;
-
-    @Shadow private BlockPos currentBlock;
-
-    /**
-     * @author a
-     * @reason b
-     */
-    @Overwrite
-    private boolean isHittingPosition(BlockPos pos) {
-        return NoBreakReset.isHittingPositionHook(pos, currentItemHittingBlock, currentBlock);
-    }
 
     @Inject(method = "windowClick", at = @At(value = "HEAD"), cancellable = true)
     private void onWindowClick(int windowId, int slotId, int mouseButtonClicked, int mode, EntityPlayer playerIn, CallbackInfoReturnable<ItemStack> cir) {
---
+++ odinclient/src/main/kotlin/me/odinclient/features/impl/floor7/p3/QueueTerms.kt
@@ -0,0 +1,62 @@
+package me.odinclient.features.impl.floor7.p3
+
+import me.odinclient.utils.skyblock.PlayerUtils
+import me.odinmain.events.impl.GuiEvent
+import me.odinmain.events.impl.TerminalClosedEvent
+import me.odinmain.features.Category
+import me.odinmain.features.Module
+import me.odinmain.features.impl.floor7.p3.TerminalSolver
+import me.odinmain.features.impl.floor7.p3.TerminalTypes
+import me.odinmain.features.settings.impl.DualSetting
+import me.odinmain.features.settings.impl.NumberSetting
+import me.odinmain.utils.clock.Clock
+import me.odinmain.utils.skyblock.modMessage
+import net.minecraft.client.gui.inventory.GuiChest
+import net.minecraft.client.gui.inventory.GuiContainer
+import net.minecraft.inventory.ContainerChest
+import net.minecraftforge.fml.common.eventhandler.Event
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent
+
+object QueueTerms : Module(
+    name = "Queue Terms",
+    category = Category.FLOOR7,
+    description = "Queues clicks in terminals to ensure every click is registered."
+) {
+    private data class Click(val slot: Int, val mode: Int, val button: Int)
+    private var clickedThisWindow = false
+    private val queue = mutableListOf<Click>()
+
+
+    @SubscribeEvent
+    fun onGuiOpen(event: GuiEvent.GuiLoadedEvent) {
+        if (queue.isNotEmpty()) {
+            val container = mc.thePlayer?.openContainer ?: return
+            val click = queue.firstOrNull() ?: return
+            PlayerUtils.windowClick(slotId = click.slot, button = click.button, mode = click.mode)
+            queue.removeFirst()
+            clickedThisWindow = true
+        } else clickedThisWindow = false
+    }
+
+    @SubscribeEvent
+    fun onTerminalLeft(event: TerminalClosedEvent) {
+        clickedThisWindow = false
+        queue.clear()
+    }
+
+    @SubscribeEvent
+    fun onMouseClick(event: GuiEvent.GuiMouseClickEvent) {
+        modMessage("nigger")
+        if (TerminalSolver.currentTerm == TerminalTypes.NONE ) return
+        if (!clickedThisWindow) {
+            clickedThisWindow = true
+            return
+        }
+
+        event.isCanceled = true
+        clickedThisWindow = true
+        queue.add(Click(slot = (event.gui as? GuiContainer)?.slotUnderMouse?.slotIndex ?: return, mode = if (event.button == 2) 3 else 0, button = event.button))
+        modMessage("added ${queue.last()}")
+    }
+}
