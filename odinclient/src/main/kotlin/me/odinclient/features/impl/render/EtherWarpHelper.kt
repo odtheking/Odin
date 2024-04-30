21542b6e6c6e0ec08e7e12076636c6c5dd8bb563
odtheking <ohad.dotan@gmail.com>
2024-04-30 21:59:43 +0300

    Rendering for azoo bloo

--- odinclient/src/main/kotlin/me/odinclient/features/impl/render/EtherWarpHelper.kt
+++ odinclient/src/main/kotlin/me/odinclient/features/impl/render/EtherWarpHelper.kt
@@ -8,10 +8,7 @@ import me.odinmain.features.Module
 import me.odinmain.features.impl.dungeon.DungeonWaypoints.toBlockPos
 import me.odinmain.features.impl.dungeon.DungeonWaypoints.toVec3
 import me.odinmain.features.settings.Setting.Companion.withDependency
-import me.odinmain.features.settings.impl.BooleanSetting
-import me.odinmain.features.settings.impl.ColorSetting
-import me.odinmain.features.settings.impl.DualSetting
-import me.odinmain.features.settings.impl.NumberSetting
+import me.odinmain.features.settings.impl.*
 import me.odinmain.ui.clickgui.util.ColorUtil.withAlpha
 import me.odinmain.utils.PositionLook
 import me.odinmain.utils.clock.Clock
@@ -44,8 +41,8 @@ object EtherWarpHelper : Module(
     private val renderFail: Boolean by BooleanSetting("Show when failed", true).withDependency { render }
     private val renderColor: Color by ColorSetting("Color", Color.ORANGE.withAlpha(.5f), allowAlpha = true).withDependency { render }
     private val wrongColor: Color by ColorSetting("Wrong Color", Color.RED.withAlpha(.5f), allowAlpha = true).withDependency { renderFail }
-    private val filled: Boolean by DualSetting("Type", "Outline", "Filled", default = false).withDependency { render }
-    private val thickness: Float by NumberSetting("Thickness", 3f, 1f, 10f, .1f).withDependency { !filled && render }
+    private val style: Int by SelectorSetting("Style", "Filled", arrayListOf("Filled", "Outline", "Filled Outline"), description = "Whether or not the box should be filled.")
+    private val thickness: Float by NumberSetting("Thickness", 3f, 1f, 10f, .1f).withDependency { render }
     private val phase: Boolean by BooleanSetting("Phase", false).withDependency { render }
     private val etherWarpTriggerBot: Boolean by BooleanSetting("Trigger Bot", false, description = "Uses Dungeon Waypoints to trigger bot to the closest waypoint.")
     private val etherWarpTBDelay: Long by NumberSetting("Trigger Bot Delay", 200L, 0, 1000, 10).withDependency { etherWarpTriggerBot }
@@ -82,7 +79,7 @@ object EtherWarpHelper : Module(
             getBlockAt(pos).setBlockBoundsBasedOnState(mc.theWorld, pos)
             val aabb = getBlockAt(pos).getSelectedBoundingBox(mc.theWorld, pos) ?: return
 
-            Renderer.drawBox(aabb, color, outlineWidth = thickness, depth = phase, outlineAlpha = if (filled) 0 else 1, fillAlpha = if (filled) 1 else 0)
+            Renderer.drawBox(aabb, color, outlineWidth = thickness, depth = phase, outlineAlpha = if (style == 0) 0 else color.alpha, fillAlpha = if (style == 1) 0 else color.alpha)
         }
     }
 
