33c00bdd0882ed2dc4d81f87c9b609fde8351f1f
SubAt0mic <pokemonjunkbox@gmail.com>
2024-05-03 16:17:54 -0400

    a

--- odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/DragonTracer.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/DragonTracer.kt
@@ -1,6 +1,6 @@
 package me.odinmain.features.impl.floor7
 
-import me.odinmain.OdinMain
+import me.odinmain.OdinMain.mc
 import me.odinmain.features.impl.floor7.WitherDragons.tracerThickness
 import me.odinmain.utils.addVec
 import me.odinmain.utils.fastEyeHeight
@@ -10,6 +10,6 @@ import me.odinmain.utils.render.Renderer
 
 object DragonTracer {
     fun renderTracers(dragon: WitherDragonsEnum) {
-        if (dragon.spawnTime() > 0) Renderer.draw3DLine(OdinMain.mc.thePlayer.renderVec.addVec(y = fastEyeHeight()), dragon.spawnPos.addVec(0.5, 3.5, 0.5), dragon.color, tracerThickness)
+        if (dragon.spawnTime() > 0) Renderer.draw3DLine(mc.thePlayer.renderVec.addVec(y = fastEyeHeight()), dragon.spawnPos.addVec(0.5, 3.5, 0.5), dragon.color, tracerThickness)
     }
 }
