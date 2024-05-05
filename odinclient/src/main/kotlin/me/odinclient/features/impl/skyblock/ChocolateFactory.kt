d8ac752e8097f23dc917493b29536021116edcec
Jurre Cottyn <jurre.cottyn@student.howest.be>
2024-05-05 19:26:13 +0200

    Changed get skull ~ Needs testing on main server

--- odinclient/src/main/kotlin/me/odinclient/features/impl/skyblock/ChocolateFactory.kt
+++ odinclient/src/main/kotlin/me/odinclient/features/impl/skyblock/ChocolateFactory.kt
@@ -5,25 +5,23 @@ import me.odinmain.features.Category
 import me.odinmain.features.Module
 import me.odinmain.features.settings.impl.BooleanSetting
 import me.odinmain.features.settings.impl.NumberSetting
+import me.odinmain.utils.BunnyEggTextures
 import me.odinmain.utils.name
 import me.odinmain.utils.noControlCodes
 import me.odinmain.utils.render.Color
 import me.odinmain.utils.render.Renderer
 import me.odinmain.utils.skyblock.PlayerUtils.windowClick
+import me.odinmain.utils.skyblock.getSkullValue
 import me.odinmain.utils.skyblock.lore
 import me.odinmain.utils.skyblock.modMessage
 import net.minecraft.entity.item.EntityArmorStand
 import net.minecraft.inventory.Container
 import net.minecraft.inventory.ContainerChest
-import net.minecraft.item.ItemSkull
 import net.minecraft.util.Vec3
 import net.minecraftforge.client.event.RenderWorldLastEvent
 import net.minecraftforge.client.event.sound.PlaySoundEvent
-import net.minecraftforge.common.util.Constants
-import net.minecraftforge.event.world.WorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
-import java.util.*
 
 
 object ChocolateFactory : Module(
@@ -42,13 +40,18 @@ object ChocolateFactory : Module(
     private var chocoProduction = 0f
 
     val indexToName = mapOf(29 to "Bro", 30 to "Cousin", 31 to "Sis", 32 to "Daddy", 33 to "Granny")
-    private val EGG_TEXTURES = arrayOf(
-        "ewogICJ0aW1lc3RhbXAiIDogMTcxMTQ2MjY0OTcwMSwKICAicHJvZmlsZUlkIiA6ICI3NGEwMzQxNWY1OTI0ZTA4YjMyMGM2MmU1NGE3ZjJhYiIsCiAgInByb2ZpbGVOYW1lIiA6ICJNZXp6aXIiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTVlMzYxNjU4MTlmZDI4NTBmOTg1NTJlZGNkNzYzZmY5ODYzMTMxMTkyODNjMTI2YWNlMGM0Y2M0OTVlNzZhOCIKICAgIH0KICB9Cn0",
-        "ewogICJ0aW1lc3RhbXAiIDogMTcxMTQ2MjU2ODExMiwKICAicHJvZmlsZUlkIiA6ICI3NzUwYzFhNTM5M2Q0ZWQ0Yjc2NmQ4ZGUwOWY4MjU0NiIsCiAgInByb2ZpbGVOYW1lIiA6ICJSZWVkcmVsIiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzdhZTZkMmQzMWQ4MTY3YmNhZjk1MjkzYjY4YTRhY2Q4NzJkNjZlNzUxZGI1YTM0ZjJjYmM2NzY2YTAzNTZkMGEiCiAgICB9CiAgfQp9",
-        "ewogICJ0aW1lc3RhbXAiIDogMTcxMTQ2MjY3MzE0OSwKICAicHJvZmlsZUlkIiA6ICJiN2I4ZTlhZjEwZGE0NjFmOTY2YTQxM2RmOWJiM2U4OCIsCiAgInByb2ZpbGVOYW1lIiA6ICJBbmFiYW5hbmFZZzciLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTQ5MzMzZDg1YjhhMzE1ZDAzMzZlYjJkZjM3ZDhhNzE0Y2EyNGM1MWI4YzYwNzRmMWI1YjkyN2RlYjUxNmMyNCIKICAgIH0KICB9Cn0"
-    )
-    private var eggList: ArrayList<EntityArmorStand> = ArrayList()
 
+    private var eggList = mutableListOf<Egg>()
+
+    private enum class ChocolateEggs (
+        val texture: String, val type: String, val color: Color
+    ) {
+        Breakfast   (BunnyEggTextures.breakfastEggTexture, "§6Breakfast Egg", Color.ORANGE),
+        Lunch       (BunnyEggTextures.lunchEggTexture,   "§9Lunch Egg ", Color.BLUE),
+        Dinner      (BunnyEggTextures.dinnerEggTexture,     "§aDinner Egg", Color.GREEN),
+    }
+
+    data class Egg(val entity: EntityArmorStand, val renderName: String, val color: Color)
 
     init {
         onWorldLoad { eggList.clear() }
@@ -124,44 +127,22 @@ object ChocolateFactory : Module(
         }
         val entityArmorStands = mc.theWorld.loadedEntityList.filterIsInstance<EntityArmorStand>()
         if (event.phase == TickEvent.Phase.END) return
-
         for (stand in entityArmorStands) {
-            if(matchSkullTexture(stand, *EGG_TEXTURES)){
-                if(stand !in eggList) eggList.add(stand)
-                if(eggList.size > 3) eggList.clear()
-            }
+            val texture = getSkullValue(stand)
+            modMessage(texture)
+            val egg = ChocolateEggs.entries.firstOrNull { it.texture == texture } ?: return
+            val egg2 = Egg(stand, egg.type, egg.color)
+            if(egg2 !in eggList) eggList.add(egg2)
         }
+        if(eggList.size > 3) eggList.clear()
     }
 
     @SubscribeEvent
     fun onRenderWorld(event: RenderWorldLastEvent) {
         if (eggList.isEmpty()) return
-        for(egg in eggList){
-            var location = Vec3(egg.posX - 0.5, egg.posY + 1.47, egg.posZ - .5)
-            Renderer.drawCustomBeacon("§6Egg§r", location, Color.GREEN, increase = true, beacon = false)
-
+        for (egg in eggList) {
+            val location = Vec3(egg.entity.posX, egg.entity.posY, egg.entity.posZ)
+            Renderer.drawCustomBeacon(egg.renderName, location, egg.color, increase = true, beacon = false)
         }
-    }
-
-    /*
-     * stuff from GumTuneClient
-     * https://github.com/RoseGoldIsntGay/GumTuneClient/blob/main/src/main/java/rosegold/gumtuneclient/modules/render/ESPs.java#L307
-     */
-    private fun matchSkullTexture(entity: EntityArmorStand, vararg skullTextures: String): Boolean {
-        val helmetItemStack = entity.getCurrentArmor(3)
-        if (helmetItemStack != null && helmetItemStack.item is ItemSkull) {
-            val textures = helmetItemStack.serializeNBT().getCompoundTag("tag").getCompoundTag("SkullOwner")
-                .getCompoundTag("Properties").getTagList("textures", Constants.NBT.TAG_COMPOUND)
-            for (i in 0 until textures.tagCount()) {
-                val finalI = i
-                if (Arrays.stream(skullTextures).anyMatch { s ->
-                        textures.getCompoundTagAt(finalI).getString("Value") == s
-                    }) {
-                    return true
-                }
-            }
-        }
-
-        return false
     }
 }
