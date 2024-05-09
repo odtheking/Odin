7f992ff10e24f932393f475f6ffc5363e5c4ffed
SubAt0mic <pokemonjunkbox@gmail.com>
2024-05-09 10:59:41 -0400

    better

--- odinmain/src/main/kotlin/me/odinmain/commands/impl/PosMsgCommand.kt
+++ odinmain/src/main/kotlin/me/odinmain/commands/impl/PosMsgCommand.kt
@@ -1,5 +1,6 @@
 package me.odinmain.commands.impl
 
+import codes.som.anthony.koffee.types.double
 import com.github.stivais.commodore.utils.GreedyString
 import me.odinmain.OdinMain.mc
 import me.odinmain.commands.commodore
@@ -9,24 +10,32 @@ import me.odinmain.utils.round
 import me.odinmain.utils.skyblock.modMessage
 
 val PosMsgCommand = commodore("posmsg") {
-    literal("add").runs { x: Double, y: Double, z: Double, delay: Long, message: GreedyString ->
-        val saveData = "x: ${x}, y: ${y}, z: ${z}, delay: ${delay}, message: \"${message}\""
-        if (posMessageStrings.contains(saveData)) modMessage("This message already exists!")
-        modMessage("Message \"${message}\" added at $x, $y, $z, with ${delay}ms delay")
-        posMessageStrings.add(saveData)
-        Config.save()
-    }
-
-    literal("addatpos").runs { delay: Long, message: GreedyString ->
-        val saveData = "x: ${mc.thePlayer.posX.round(2)}, y: ${mc.thePlayer.posY.round(2)}, z: ${mc.thePlayer.posZ.round(2)}, delay: ${delay}, message: \"${message}\""
-        if (posMessageStrings.contains(saveData)) modMessage("This message already exists!")
-        modMessage("Message \"${message}\" added at ${mc.thePlayer.posX.round(2)}, ${mc.thePlayer.posY.round(2)}, ${mc.thePlayer.posZ.round(2)}, with ${delay}ms delay")
-        posMessageStrings.add(saveData)
-        Config.save()
+    literal("add") {
+        literal("at").runs { x: Double, y: Double, z: Double, delay: Long, distance: Double, message: GreedyString ->
+            val saveData = "x: ${x}, y: ${y}, z: ${z}, delay: ${delay}, distance: ${distance}, message: \"${message}\""
+            if (posMessageStrings.contains(saveData)) modMessage("This message already exists!")
+            modMessage("Message \"${message}\" added at $x, $y, $z, with ${delay}ms delay, triggered up to $distance blocks away.")
+            posMessageStrings.add(saveData)
+            Config.save()
+        }
+        literal("in").runs { x: Double, y: Double, z: Double, x2: Double, y2: Double, z2: Double, delay: Long, message: GreedyString ->
+            val saveData = "x: ${x}, y: ${y}, z: ${z}, x2: ${x2}, y2: ${y2}, z2: ${z2}, delay: ${delay}, message: \"${message}\""
+            if (posMessageStrings.contains(saveData)) modMessage("This message already exists!")
+            modMessage("Message \"${message}\" added in $x, $y, $z, $x2, $y2, $z2, with ${delay}ms delay.")
+            posMessageStrings.add(saveData)
+            Config.save()
+        }
+        literal("atself").runs { delay: Long, distance: Double, message: GreedyString ->
+            val saveData = "x: ${mc.thePlayer.posX.round(2)}, y: ${mc.thePlayer.posY.round(2)}, z: ${mc.thePlayer.posZ.round(2)}, delay: ${delay}, distance: ${distance}, message: \"${message}\""
+            if (posMessageStrings.contains(saveData)) modMessage("This message already exists!")
+            modMessage("Message \"${message}\" added at ${mc.thePlayer.posX.round(2)}, ${mc.thePlayer.posY.round(2)}, ${mc.thePlayer.posZ.round(2)}, with ${delay}ms delay, triggered up to $distance blocks away.")
+            posMessageStrings.add(saveData)
+            Config.save()
+        }
     }
 
     literal("remove").runs { index: Int ->
-        if (posMessageStrings.getOrNull(index) == null) return@runs modMessage("Theres no message in position #$index")
+        if (posMessageStrings.getOrNull(index-1) == null) return@runs modMessage("Theres no message in position #$index")
         modMessage("Removed Positional Message #$index")
         posMessageStrings.removeAt(index-1)
         Config.save()
--- odinmain/src/main/kotlin/me/odinmain/features/impl/dungeon/PosMessages.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/dungeon/PosMessages.kt
@@ -5,11 +5,13 @@ import me.odinmain.features.Module
 import me.odinmain.events.impl.PacketSentEvent
 import me.odinmain.features.settings.impl.BooleanSetting
 import me.odinmain.features.settings.impl.ListSetting
+import me.odinmain.utils.isVecInXZ
 import me.odinmain.utils.skyblock.LocationUtils
 import me.odinmain.utils.skyblock.dungeon.DungeonUtils
 import me.odinmain.utils.skyblock.modMessage
 import me.odinmain.utils.skyblock.partyMessage
 import net.minecraft.network.play.client.C03PacketPlayer.C04PacketPlayerPosition
+import net.minecraft.util.AxisAlignedBB
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.util.*
 import kotlin.concurrent.schedule
@@ -21,37 +23,74 @@ object PosMessages : Module(
 ) {
     private val onlyDungeons: Boolean by BooleanSetting("Only in Dungeons", true, description = "Only sends messages when you're in a dungeon.")
 
-    data class PosMessage(val x: Double, val y: Double, val z: Double, val delay: Long, val message: String)
+    data class PosMessage(val x: Double, val y: Double, val z: Double, val x2: Double?, val y2: Double?, val z2: Double?, val delay: Long, val distance: Double?, val message: String)
     val posMessageStrings: MutableList<String> by ListSetting("Pos Messages Strings", mutableListOf())
     private val sentMessages = mutableMapOf<String, Boolean>()
 
 
     @SubscribeEvent
-    fun onPosUpdate(event: PacketSentEvent) {
-        if (event.packet !is C04PacketPlayerPosition || (onlyDungeons && !DungeonUtils.inDungeons) || !LocationUtils.inSkyblock) return
+    fun posMessageSend(event: PacketSentEvent) {
+        //if (event.packet !is C04PacketPlayerPosition || (onlyDungeons && !DungeonUtils.inDungeons) || !LocationUtils.inSkyblock) return
         posMessageStrings.forEach {
-            val msg = parsePosString(it) ?: return@forEach
-            val messageSent = sentMessages.getOrDefault(it, false)
-            if (mc.thePlayer != null && mc.thePlayer.getDistance(msg.x, msg.y, msg.z) <= 1) {
-                if (!messageSent) Timer().schedule(msg.delay) {
-                    if (mc.thePlayer.getDistance(msg.x, msg.y, msg.z) <= 1)
-                    partyMessage(msg.message)
-                }
-                sentMessages[it] = true
-            } else sentMessages[it] = false
+            findParser(it)
         }
     }
 
+    private fun findParser(posMessageString: String) {
+        val atRegex = Regex("x: (.*), y: (.*), z: (.*), delay: (.*), distance: (.*), message: \"(.*)\"")
+        val inRegex = Regex("x: (.*), y: (.*), z: (.*), x2: (.*), y2: (.*), z2: (.*), delay: (.*), message: \"(.*)\"")
+        if (posMessageString.matches(atRegex)) handleAtString(posMessageString) else handleInString(posMessageString)
+    }
 
-    private fun parsePosString(posMessageString: String): PosMessage? {
-        val regex = Regex("x: (.*), y: (.*), z: (.*), delay: (.*), message: \"(.*)\"")
+    private fun handleAtString(posMessageString: String) {
+        val parsedmsg = parseAtString(posMessageString) ?: return
+        val msgSent = sentMessages.getOrDefault(posMessageString, false)
+        if (mc.thePlayer != null && mc.thePlayer.getDistance(parsedmsg.x, parsedmsg.y, parsedmsg.z) <= (parsedmsg.distance ?: return)) {
+            if (!msgSent) Timer().schedule(parsedmsg.delay) {
+                if (mc.thePlayer.getDistance(parsedmsg.x, parsedmsg.y, parsedmsg.z) <= parsedmsg.distance)
+                    partyMessage(parsedmsg.message)
+            }
+            sentMessages[posMessageString] = true
+        } else sentMessages[posMessageString] = false
+    }
+
+    private fun handleInString(posMessageString: String) {
+        val parsedmsg = parseInString(posMessageString) ?: return
+        val msgSent = sentMessages.getOrDefault(posMessageString, false)
+        if (mc.thePlayer != null && isVecInXZ(mc.thePlayer.positionVector, AxisAlignedBB(parsedmsg.x, parsedmsg.y, parsedmsg.z, parsedmsg.x2 ?: return, parsedmsg.y2 ?: return, parsedmsg.z2 ?: return))) {
+            if (!msgSent) Timer().schedule(parsedmsg.delay) {
+                if (isVecInXZ(mc.thePlayer.positionVector, AxisAlignedBB(parsedmsg.x, parsedmsg.y, parsedmsg.z, parsedmsg.x2, parsedmsg.y2, parsedmsg.z2))) {
+                    partyMessage(parsedmsg.message)
+                }
+            }
+            sentMessages[posMessageString] = true
+        } else sentMessages[posMessageString] = false
+    }
+
+    private fun parseAtString(posMessageString: String): PosMessage? {
+        val regex = Regex("x: (.*), y: (.*), z: (.*), delay: (.*), distance: (.*), message: \"(.*)\"")
         val matchResult = regex.matchEntire(posMessageString) ?: return null
-        val (x, y, z, delay, message) = matchResult.destructured
+        val (x, y, z, delay, distance, message) = matchResult.destructured
         val xDouble = x.toDoubleOrNull() ?: return null.also { modMessage("Failed to parse x: $x") }
         val yDouble = y.toDoubleOrNull() ?: return null.also { modMessage("Failed to parse y: $y") }
         val zDouble = z.toDoubleOrNull() ?: return null.also { modMessage("Failed to parse z: $z") }
         val delayLong = delay.toLongOrNull() ?: return null.also { modMessage("Failed to parse delay: $delay") }
-        return PosMessage(xDouble, yDouble, zDouble, delayLong, message)
+        val distanceDouble = distance.toDoubleOrNull() ?: return null.also { modMessage("Failed to parse distance: $distance")}
+        return PosMessage(xDouble, yDouble, zDouble, null, null, null, delayLong, distanceDouble, message)
+    }
+
+    private fun parseInString(posMessageString: String): PosMessage? {
+        val inRegex = Regex("x: (.*), y: (.*), z: (.*), x2: (.*), y2: (.*), z2: (.*), delay: (.*), message: \"(.*)\"")
+        val matchResult = inRegex.matchEntire(posMessageString) ?: return null
+        val (x, y, z, x2, y2, z2, delay, message) = matchResult.destructured
+        val xDouble = x.toDoubleOrNull() ?: return null.also { modMessage("Failed to parse x: $x") }
+        val yDouble = y.toDoubleOrNull() ?: return null.also { modMessage("Failed to parse y: $y") }
+        val zDouble = z.toDoubleOrNull() ?: return null.also { modMessage("Failed to parse z: $z") }
+        val x2Double = x2.toDoubleOrNull() ?: return null.also { modMessage("Failed to parse x2: $x2") }
+        val y2Double = y2.toDoubleOrNull() ?: return null.also { modMessage("Failed to parse y2: $y2") }
+        val z2Double = z2.toDoubleOrNull() ?: return null.also { modMessage("Failed to parse z2: $z2") }
+        val delayLong = delay.toLongOrNull() ?: return null.also { modMessage("Failed to parse delay: $delay") }
+        return PosMessage(xDouble, yDouble, zDouble, x2Double, y2Double, z2Double, delayLong, null, message)
     }
 
 }
