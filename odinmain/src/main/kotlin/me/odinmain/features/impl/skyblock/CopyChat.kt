f1c6c71fd0ce00913ab181b904fe052ae78dbddd
odtheking <ohad.dotan@gmail.com>
2024-05-12 12:14:49 +0300

    Fixed issue with CopyChat

--- odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/CopyChat.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/CopyChat.kt
@@ -3,12 +3,15 @@ package me.odinmain.features.impl.skyblock
 import me.odinmain.events.impl.GuiEvent
 import me.odinmain.features.Category
 import me.odinmain.features.Module
+import me.odinmain.features.settings.impl.KeybindSetting
+import me.odinmain.features.settings.impl.Keybinding
 import me.odinmain.utils.copyToClipboard
 import me.odinmain.utils.noControlCodes
 import me.odinmain.utils.render.scaleFactor
 import me.odinmain.utils.skyblock.modMessage
 import net.minecraft.client.gui.GuiChat
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import org.lwjgl.input.Keyboard
 import org.lwjgl.input.Mouse
 import kotlin.math.floor
 
@@ -17,6 +20,8 @@ object CopyChat : Module(
     category = Category.SKYBLOCK,
     description = "Allows you to right click messages in chat to copy them.",
 ) {
+    private val keybind: Keybinding by KeybindSetting("Keybind", Keyboard.KEY_LCONTROL, "Hold to copy message with color codes")
+
     @SubscribeEvent
     fun mouseClicked(event: GuiEvent.GuiMouseClickEvent) {
         if (event.button != 1 || mc.currentScreen !is GuiChat) return
@@ -29,10 +34,12 @@ object CopyChat : Module(
         val components = mutableSetOf<String>()
 
         for (x in 0 until maxChatWidth step 10) {
-            val scannedComponent = chatGui.getChatComponent(x, my)?.unformattedTextForChat?.noControlCodes ?: continue
+            val scannedComponent = chatGui.getChatComponent(x, my)?.unformattedTextForChat ?: continue
             components.add(scannedComponent)
         }
-        copyToClipboard(components.joinToString { it })
+        val toCopy = components.joinToString(separator = "") { it }
+
+        copyToClipboard(if (keybind.isDown()) toCopy else toCopy.noControlCodes)
         modMessage("§aCopied chat message to clipboard!")
     }
 }
