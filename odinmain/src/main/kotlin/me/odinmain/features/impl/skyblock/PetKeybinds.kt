1cc91b9b1a3d11b641662849b08d6b1ff186baaf
SubAt0mic <pokemonjunkbox@gmail.com>
2024-06-16 16:32:22 -0400

    cancelling before redirecting might not be legit?

--- odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/PetKeybinds.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/PetKeybinds.kt
@@ -59,11 +59,11 @@ object PetKeybinds : Module(
             }
         }
 
-        event.isCanceled = true
         if (nounequip && getItemIndexInContainerChestByLore(chest, "§7§cClick to despawn!", 10..43) == index && !unequipKeybind.isDown()) return modMessage("That pet is already equipped!")
         if (!clickCoolDown.hasTimePassed() || index == null) return
         if (index > chest.lowerChestInventory.sizeInventory - 1 || index < 1) return modMessage("Invalid index. $index, ${chest.name}")
         mc.playerController.windowClick(chest.windowId, index, 0, 0, mc.thePlayer)
         clickCoolDown.update()
+        event.isCanceled = true
     }
 }
