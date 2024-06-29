dda51d587b345808e45f68258873979b4a2e40e1
SubAt0mic <pokemonjunkbox@gmail.com>
2024-06-29 19:08:30 -0400

    better mimic detection probably?

--- odinmain/src/main/kotlin/me/odinmain/features/impl/dungeon/Mimic.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/dungeon/Mimic.kt
@@ -1,6 +1,7 @@
 package me.odinmain.features.impl.dungeon
 
 import me.odinmain.OdinMain.isLegitVersion
+import me.odinmain.events.impl.BlockChangeEvent
 import me.odinmain.events.impl.RenderChestEvent
 import me.odinmain.features.Category
 import me.odinmain.features.Module
@@ -14,6 +15,9 @@ import me.odinmain.utils.skyblock.dungeon.DungeonUtils
 import me.odinmain.utils.skyblock.partyMessage
 import me.odinmain.utils.toAABB
 import net.minecraft.entity.monster.EntityZombie
+import net.minecraft.init.Blocks
+import net.minecraft.network.play.server.S23PacketBlockChange
+import net.minecraft.util.BlockPos
 import net.minecraftforge.event.entity.living.LivingDeathEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -24,25 +28,53 @@ object Mimic : Module(
 ) {
     private val mimicMessageToggle: Boolean by BooleanSetting("Toggle Mimic Message", default = true)
     val mimicMessage: String by StringSetting("Mimic Message", "Mimic Killed!", 128, description = "Message sent when mimic is detected as killed").withDependency { mimicMessageToggle }
-    val reset: () -> Unit by ActionSetting("Send message", description = "Sends Mimic killed message in party chat.") { partyMessage(mimicMessage) }
+    val reset: () -> Unit by ActionSetting("Send message", description = "Sends Mimic killed message in party chat.") { mimicKilled() }
     private val mimicBox: Boolean by BooleanSetting("Mimic Box", false, description = "Draws a box around the mimic chest.")
     private val style: Int by SelectorSetting("Style", Renderer.defaultStyle, Renderer.styles, description = Renderer.styleDesc).withDependency { mimicBox }
     private val color: Color by ColorSetting("Color", Color.RED.withAlpha(0.5f), allowAlpha = true, description = "The color of the box.").withDependency { mimicBox }
     private val lineWidth: Float by NumberSetting("Line Width", 2f, 0.1f, 10f, 0.1f, description = "The width of the box's lines.").withDependency { mimicBox }
     private val depthCheck: Boolean by BooleanSetting("Depth check", false, description = "Boxes show through walls.").withDependency { mimicBox }
 
+    private var chestUpdate: Long = 0
+    private var pos: BlockPos? = null
+
+    init {
+        execute(100) {
+            if (!DungeonUtils.inDungeons || chestUpdate == 0L || pos == null || DungeonUtils.mimicKilled ||
+                System.currentTimeMillis() - chestUpdate > 600 || mc.thePlayer.getDistanceSq(pos) > 400 ) return@execute
+            if (mc.theWorld.loadedEntityList.find { it is EntityZombie && it.isChild && (0..3).all { i -> it.getCurrentArmor(i) == null } } == null) mimicKilled()
+        }
+
+        onWorldLoad {
+            chestUpdate = 0L
+            pos = null
+        }
+    }
+
+    @SubscribeEvent
+    fun onBlockUpdate(event: BlockChangeEvent) {
+        if (event.old != Blocks.trapped_chest || event.update != Blocks.air ) return
+        chestUpdate = System.currentTimeMillis()
+        pos = event.pos
+    }
+
     @SubscribeEvent
     fun onEntityDeath(event: LivingDeathEvent) {
         val entity = event.entity
         if (!DungeonUtils.inDungeons || entity !is EntityZombie || !entity.isChild || !(0..3).all { entity.getCurrentArmor(it) == null }) return
-        if (mimicMessageToggle) partyMessage(mimicMessage)
-        currentDungeon?.dungeonStats?.mimicKilled = true
+        mimicKilled()
     }
 
     @SubscribeEvent
     fun onRenderLast(event: RenderChestEvent.Post) {
         if (event.chest.chestType != 1 || !DungeonUtils.inDungeons || DungeonUtils.inBoss || !mimicBox) return
         Renderer.drawStyledBox(event.chest.pos.toAABB(), color, style, lineWidth, isLegitVersion)
+    }
+
+    private fun mimicKilled() {
+        if (DungeonUtils.mimicKilled) return
+        if (mimicMessageToggle) partyMessage(mimicMessage)
+        currentDungeon?.dungeonStats?.mimicKilled = true
     }
 
     override fun onKeybind() {
