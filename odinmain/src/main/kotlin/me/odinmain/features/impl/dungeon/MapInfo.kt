b00f85374828891d1e6e915bcd22af97c1921492
SubAt0mic <pokemonjunkbox@gmail.com>
2024-07-02 15:50:11 -0400

    added a background option

--- odinmain/src/main/kotlin/me/odinmain/features/impl/dungeon/MapInfo.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/dungeon/MapInfo.kt
@@ -2,14 +2,14 @@ package me.odinmain.features.impl.dungeon
 
 import me.odinmain.features.Category
 import me.odinmain.features.Module
-import me.odinmain.features.settings.impl.BooleanSetting
-import me.odinmain.features.settings.impl.DualSetting
-import me.odinmain.features.settings.impl.HudSetting
-import me.odinmain.features.settings.impl.SelectorSetting
+import me.odinmain.features.settings.Setting.Companion.withDependency
+import me.odinmain.features.settings.impl.*
+import me.odinmain.ui.clickgui.util.ColorUtil.withAlpha
 import me.odinmain.ui.hud.HudElement
 import me.odinmain.utils.render.Color
 import me.odinmain.utils.render.getMCTextWidth
 import me.odinmain.utils.render.mcText
+import me.odinmain.utils.render.roundedRectangle
 import me.odinmain.utils.skyblock.dungeon.DungeonUtils
 
 object MapInfo : Module(
@@ -21,6 +21,8 @@ object MapInfo : Module(
     private val remaining: Boolean by DualSetting("Min Secrets", "Minimum", "Remaining", default = false, description = "Display minimum secrets or secrets until s+.")
     private val unknown: Boolean by DualSetting("Deaths", "Deaths", "Unknown", default = false, description = "Display deaths or unknown secrets. (Unknown secrets are secrets in rooms that haven't been discovered yet. May not be helpful in full party runs.)")
     val togglePaul: Int by SelectorSetting("Paul Settings", "Automatic", options = arrayListOf("Automatic", "Force Disable", "Force Enable"))
+    private val background: Boolean by BooleanSetting("Background", default = false, description = "Render a background behind the score info")
+    private val color: Color by ColorSetting("Background Color", default = Color.DARK_GRAY.withAlpha(0.5f), true, description = "The color of the background").withDependency { background }
 
     val hud: HudElement by HudSetting("Hud", 10f, 10f, 1f, false) {
         if (it) {
@@ -28,24 +30,26 @@ object MapInfo : Module(
             val unknownWidth = getMCTextWidth(unknownText)
             val cryptWidth = getMCTextWidth("§7Crypts: §c?")
             val scoreWidth = getMCTextWidth("§7Score: §d???")
+            if (background) roundedRectangle(0, 0, 160, 19, color, 0, 0)
             mcText("§7Secrets: §e0§7-§b?§7-§c?", 1, 1, 1f, Color.WHITE, center = false)
             mcText("§7Score: §d???", 159 - scoreWidth, 1, 1f, Color.WHITE, center = false)
             mcText(unknownText, 1, 10, 1f, Color.WHITE, center = false)
-            val centerX = (unknownWidth+(160-unknownWidth-cryptWidth)/2) - getMCTextWidth("§7Mimic: §c✘")/2
+            val centerX = (unknownWidth+1+(159-unknownWidth-cryptWidth)/2) - getMCTextWidth("§7Mimic: §c✘")/2
             mcText("§7Mimic: §c✘", centerX, 10, 1f, Color.WHITE, center = false)
             mcText("§7Crypts: §c?", 159 - cryptWidth, 10, 1f, Color.WHITE, center = false)
         } else if (DungeonUtils.inDungeons && (!disableInBoss || !DungeonUtils.inBoss)){
             val unknownWidth = getMCTextWidth(unknownSecretsText)
             val cryptWidth = getMCTextWidth(cryptText)
             val scoreWidth = getMCTextWidth(scoreText)
+            if (background) roundedRectangle(0, 0, 160, 19, color, 0, 0)
             mcText(secretText, 1, 1, 1f, Color.WHITE, center = false)
             mcText(scoreText, 159 - scoreWidth, 1, 1f, Color.WHITE, center = false)
             mcText(unknownSecretsText, 1, 10, 1f, Color.WHITE, center = false)
-            val centerX = (unknownWidth+(159-unknownWidth-cryptWidth)/2) - getMCTextWidth(mimicText)/2
+            val centerX = (unknownWidth+1+(159-unknownWidth-cryptWidth)/2) - getMCTextWidth(mimicText)/2
             mcText(mimicText, centerX, 10, 1f, Color.WHITE, center = false)
             mcText(cryptText, 159 - cryptWidth, 10, 1f, Color.WHITE, center = false)
         } else return@HudSetting 0f to 0f
-        160f to 18f
+        160f to 19f
     }
 
     private var secretText = "§7Secrets: §b0§7-§e?§7-§c?"
