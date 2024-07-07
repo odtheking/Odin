c8193635fbb26112c4d02c511deb5f483ab62d70
SubAt0mic <pokemonjunkbox@gmail.com>
2024-07-06 21:10:26 -0400

    margin

--- odinmain/src/main/kotlin/me/odinmain/features/impl/dungeon/MapInfo.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/dungeon/MapInfo.kt
@@ -6,6 +6,7 @@ import me.odinmain.features.settings.Setting.Companion.withDependency
 import me.odinmain.features.settings.impl.*
 import me.odinmain.ui.clickgui.util.ColorUtil.withAlpha
 import me.odinmain.ui.hud.HudElement
+import me.odinmain.utils.max
 import me.odinmain.utils.render.*
 import me.odinmain.utils.skyblock.dungeon.DungeonUtils
 
@@ -18,17 +19,16 @@ object MapInfo : Module(
     private val alternate: Boolean by  BooleanSetting("Flip Crypts and Score", default = false, description = "Flips crypts and score.")
     private val addRemaining: Boolean by BooleanSetting("Include remaining", default = false, description = "adds remaining to the secrets display.").withDependency { alternate }
     private val remaining: Boolean by DualSetting("Min Secrets", "Minimum", "Remaining", default = false, description = "Display minimum secrets or secrets until s+.").withDependency { !(addRemaining && alternate) }
-    private val width: Float by NumberSetting("Width", default = 165f, min = 160f, max = 200f, increment = 1f).withDependency { alternate }
+    private val width: Float by NumberSetting("Width", default = 160f, min = 160f, max = 200f, increment = 1f)
     private val unknown: Boolean by DualSetting("Deaths", "Deaths", "Unfound", default = false, description = "Display deaths or unfound secrets. (Unknown secrets are secrets in rooms that haven't been discovered yet. May not be helpful in full party runs.)")
     val togglePaul: Int by SelectorSetting("Paul Settings", "Automatic", options = arrayListOf("Automatic", "Force Disable", "Force Enable"))
     private val background: Boolean by BooleanSetting("Background", default = false, description = "Render a background behind the score info")
+    private val margin: Float by NumberSetting("Margin", default = 0f, min = 0f, max = 5f, increment = 1f).withDependency { background }
     private val color: Color by ColorSetting("Background Color", default = Color.DARK_GRAY.withAlpha(0.5f), true, description = "The color of the background").withDependency { background }
 
 
     val hud: HudElement by HudSetting("Hud", 10f, 10f, 1f, false) {
         if ((!DungeonUtils.inDungeons || (disableInBoss && DungeonUtils.inBoss)) && !it) return@HudSetting 0f to 0f
-
-        val displayWidth = if (alternate) width else 160f
 
         val scoreText = "§7Score: ${colorizeScore(DungeonUtils.score)}"
         val secretText = "§7Secrets: §b${DungeonUtils.secretCount}"+
@@ -41,16 +41,16 @@ object MapInfo : Module(
 
         val (trText, brText) = if (alternate) listOf(cryptText, scoreText) else listOf(scoreText, cryptText)
 
-        if (background) roundedRectangle(0, 0, displayWidth-1, 19, color, 0, 0)
+        if (background) roundedRectangle(-margin, 0, width-1 + (margin * 2), 19, color, 0, 0)
         val brWidth = getMCTextWidth(brText)
         val trWidth = getMCTextWidth(trText)
         mcText(secretText, 1, 1, 1f, Color.WHITE, center = false)
-        mcText(trText, displayWidth-1 - trWidth, 1, 1f, Color.WHITE, center = false)
+        mcText(trText, width-1 - trWidth, 1, 1f, Color.WHITE, center = false)
         val unknownWidth = mcTextAndWidth(unknownSecretsText, 1, 10, 1f, Color.WHITE, center = false)
-        val centerX = (unknownWidth+1+(displayWidth-1-unknownWidth-brWidth)/2) - getMCTextWidth(mimicText)/2
+        val centerX = (unknownWidth+1+(width-1-unknownWidth-brWidth)/2) - getMCTextWidth(mimicText)/2
         mcText(mimicText, centerX, 10, 1f, Color.WHITE, center = false)
-        mcText(brText, displayWidth-1 - brWidth, 10, 1f, Color.WHITE, center = false)
-        displayWidth to 19f
+        mcText(brText, width-1 - brWidth, 10, 1f, Color.WHITE, center = false)
+        width to 19f
     }
 
     private fun colorizeCrypts(count: Int): String {
