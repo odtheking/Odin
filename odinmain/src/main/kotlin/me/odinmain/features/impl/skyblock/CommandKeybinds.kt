c941e46d0c0a2776ab39e1e2a88d01a97b6f234d
freebonsai <118639445+freebonsai@users.noreply.github.com>
2024-07-16 22:26:58 +0200

    I'm commiting this because I'm tired of it sitting here and when ui branch gets merged I will (hopefully) make a nice ui for it!!

---
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/CommandKeybinds.kt
@@ -0,0 +1,25 @@
+package me.odinmain.features.impl.skyblock
+
+import me.odinmain.features.Category
+import me.odinmain.features.Module
+import me.odinmain.features.settings.impl.KeybindSetting
+import me.odinmain.features.settings.impl.Keybinding
+import me.odinmain.utils.skyblock.sendCommand
+import org.lwjgl.input.Keyboard
+
+object CommandKeybinds : Module(
+    name = "Command Keybinds",
+    description = "Various keybinds for common skyblock commands.",
+    category = Category.SKYBLOCK,
+    key = null
+) {
+    private val pets: Keybinding by KeybindSetting("Pets", Keyboard.KEY_NONE, "").onPress {
+        sendCommand("pets")
+    }
+    private val storage: Keybinding by KeybindSetting("Storage", Keyboard.KEY_NONE, "").onPress {
+        sendCommand("storage")
+    }
+    private val dhub: Keybinding by KeybindSetting("Dungeon Hub", Keyboard.KEY_NONE, "").onPress {
+        sendCommand("warp dungeon_hub")
+    }
+}
