e130836f6a5be42f3fe4bfa27615a0c4401fd7c4
SubAt0mic <pokemonjunkbox@gmail.com>
2024-07-29 19:11:07 -0400

    reworked TerminalSounds.kt

--- odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/p3/TerminalSounds.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/p3/TerminalSounds.kt
@@ -1,5 +1,6 @@
 package me.odinmain.features.impl.floor7.p3
 
+import me.odinmain.events.impl.GuiEvent
 import me.odinmain.events.impl.PacketReceivedEvent
 import me.odinmain.events.impl.TerminalSolvedEvent
 import me.odinmain.features.Category
@@ -8,8 +9,8 @@ import me.odinmain.features.impl.floor7.p3.TerminalSolver.currentTerm
 import me.odinmain.features.impl.floor7.p3.termsim.TermSimGui
 import me.odinmain.features.settings.Setting.Companion.withDependency
 import me.odinmain.features.settings.impl.*
-import me.odinmain.utils.noControlCodes
 import me.odinmain.utils.skyblock.PlayerUtils
+import net.minecraft.client.gui.inventory.GuiChest
 import net.minecraft.network.play.server.S29PacketSoundEffect
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -29,6 +30,7 @@ object TerminalSounds : Module(
     private val clickPitch: Float by NumberSetting("Click Pitch", 2f, 0, 2, .01f, description = "Pitch of the sound.").withDependency { clickSounds }
     val reset: () -> Unit by ActionSetting("Play sound") { playTerminalSound() }.withDependency { clickSounds }
     val completeSounds: Boolean by BooleanSetting("Complete Sounds", default = false, description = "Plays a sound when you complete a terminal")
+    val cancelLastClick: Boolean by BooleanSetting("Cancel Last Click", default = false, description = "cancels the last click sound instead of playing both click and completion sound.").withDependency { clickSounds && completeSounds }
     private val completedSound: Int by SelectorSetting("Sound", "mob.blaze.hit", defaultSounds, description = "Which sound to play when you complete the terminal").withDependency { completeSounds }
     private val customCompleteSound: String by StringSetting("Custom Completion Sound", "mob.blaze.hit",
         description = "Name of a custom sound to play. This is used when Custom is selected in the Sound setting.", length = 32
@@ -51,15 +53,43 @@ object TerminalSounds : Module(
                 pitch != 4.047619f ||
                 !clickSounds
             ) return
-            playTerminalSound()
             event.isCanceled = true
         }
     }
 
     @SubscribeEvent
+    fun onSlotClick(event: GuiEvent.GuiMouseClickEvent) {
+        val gui = event.gui as? GuiChest ?: return
+        if (
+            !clickSounds ||
+            currentTerm == TerminalTypes.NONE ||
+            (currentTerm != TerminalTypes.ORDER && gui.slotUnderMouse?.slotIndex !in TerminalSolver.solution) ||
+            (currentTerm == TerminalTypes.ORDER && gui.slotUnderMouse?.slotIndex != TerminalSolver.solution.first())
+            ) return
+        if (TerminalSolver.solution.size == 1) {
+            if (!cancelLastClick) playTerminalSound()
+            playCompleteSound()
+        } else playTerminalSound()
+    }
+
+    @SubscribeEvent
+    fun onCustomSlotClick(event: GuiEvent.CustomTermGuiClick) {
+        if (
+            !clickSounds ||
+            currentTerm == TerminalTypes.NONE ||
+            (currentTerm != TerminalTypes.ORDER && event.slot !in TerminalSolver.solution) ||
+            (currentTerm == TerminalTypes.ORDER && event.slot != TerminalSolver.solution.first())
+            ) return
+        if (TerminalSolver.solution.size == 1) {
+            if (!cancelLastClick) playTerminalSound()
+            playCompleteSound()
+        } else playTerminalSound()
+    }
+
+    @SubscribeEvent
     fun onTermComplete(event: TerminalSolvedEvent) {
-        if (event.type == TerminalTypes.NONE || mc.currentScreen is TermSimGui || event.playerName != mc.thePlayer?.name || !completeSounds) return
-        playCompleteSound()
+        if (event.playerName == mc.thePlayer?.name && completeSounds) return
+        mc.addScheduledTask { mc.thePlayer.playSound("note.pling", 8f, 4f) }
     }
 
     fun playCompleteSound() {
