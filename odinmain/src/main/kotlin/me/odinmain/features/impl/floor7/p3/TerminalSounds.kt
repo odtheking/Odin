cb935bc61a7534bb1deb48e66755e902d5867bbb
SubAt0mic <pokemonjunkbox@gmail.com>
2024-07-30 15:18:37 -0400

    possible vast improvement

--- odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/p3/TerminalSounds.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/floor7/p3/TerminalSounds.kt
@@ -6,7 +6,6 @@ import me.odinmain.events.impl.TerminalSolvedEvent
 import me.odinmain.features.Category
 import me.odinmain.features.Module
 import me.odinmain.features.impl.floor7.p3.TerminalSolver.currentTerm
-import me.odinmain.features.impl.floor7.p3.termsim.TermSimGui
 import me.odinmain.features.settings.Setting.Companion.withDependency
 import me.odinmain.features.settings.impl.*
 import me.odinmain.utils.skyblock.PlayerUtils
@@ -30,13 +29,14 @@ object TerminalSounds : Module(
     private val clickPitch: Float by NumberSetting("Click Pitch", 2f, 0, 2, .01f, description = "Pitch of the sound.").withDependency { clickSounds }
     val reset: () -> Unit by ActionSetting("Play sound") { playTerminalSound() }.withDependency { clickSounds }
     val completeSounds: Boolean by BooleanSetting("Complete Sounds", default = false, description = "Plays a sound when you complete a terminal")
-    val cancelLastClick: Boolean by BooleanSetting("Cancel Last Click", default = false, description = "cancels the last click sound instead of playing both click and completion sound.").withDependency { clickSounds && completeSounds }
+    private val cancelLastClick: Boolean by BooleanSetting("Cancel Last Click", default = false, description = "cancels the last click sound instead of playing both click and completion sound.").withDependency { clickSounds && completeSounds }
     private val completedSound: Int by SelectorSetting("Sound", "mob.blaze.hit", defaultSounds, description = "Which sound to play when you complete the terminal").withDependency { completeSounds }
     private val customCompleteSound: String by StringSetting("Custom Completion Sound", "mob.blaze.hit",
         description = "Name of a custom sound to play. This is used when Custom is selected in the Sound setting.", length = 32
     ).withDependency { completedSound == defaultSounds.size - 1 && completeSounds }
     private val completeVolume: Float by NumberSetting("Completion Volume", 1f, 0, 1, .01f, description = "Volume of the sound.").withDependency { completeSounds }
     private val completePitch: Float by NumberSetting("Completion Pitch", 2f, 0, 2, .01f, description = "Pitch of the sound.").withDependency { completeSounds }
+
     val playCompleteSound: () -> Unit by ActionSetting("Play sound") { playCompleteSound() }.withDependency { completeSounds }
 
     private var lastPlayed = System.currentTimeMillis()
@@ -44,57 +44,53 @@ object TerminalSounds : Module(
     @SubscribeEvent
     fun onPacket(event: PacketReceivedEvent){
         with(event.packet) {
+            //if (this is S29PacketSoundEffect) modMessage("$soundName, $volume, $pitch")
             if (
-                this !is S29PacketSoundEffect ||
-                customSound == "note.pling" ||
-                soundName != "note.pling" ||
-                volume != 8f ||
-                pitch != 4.047619f ||
-                !clickSounds
-            ) return
-            event.isCanceled = true
+                this is S29PacketSoundEffect &&
+                soundName == "note.pling" &&
+                volume == 8f &&
+                //pitch == 1.8888888f &&
+                pitch == 4.047619f &&
+                shouldReplaceSounds()
+            ) event.isCanceled = true
         }
     }
 
     @SubscribeEvent
     fun onSlotClick(event: GuiEvent.GuiMouseClickEvent) {
-        val gui = event.gui as? GuiChest ?: return
-        if (
-            !clickSounds ||
-            currentTerm == TerminalTypes.NONE ||
-            (currentTerm != TerminalTypes.ORDER && gui.slotUnderMouse?.slotIndex !in TerminalSolver.solution) ||
-            (currentTerm == TerminalTypes.ORDER && gui.slotUnderMouse?.slotIndex != TerminalSolver.solution.first())
-            ) return
-        if (TerminalSolver.solution.size == 1 && completeSounds) {
-            if (!cancelLastClick) playTerminalSound()
-            playCompleteSound()
-        } else playTerminalSound()
+        if (!shouldReplaceSounds()) return
+        val slot = (event.gui as? GuiChest)?.slotUnderMouse?.slotIndex ?: return
+        clickSlot(slot)
     }
 
     @SubscribeEvent
     fun onCustomSlotClick(event: GuiEvent.CustomTermGuiClick) {
-        if (
-            !clickSounds ||
-            currentTerm == TerminalTypes.NONE ||
-            (currentTerm != TerminalTypes.ORDER && event.slot !in TerminalSolver.solution) ||
-            (currentTerm == TerminalTypes.ORDER && event.slot != TerminalSolver.solution.first())
-            ) return
-        if (TerminalSolver.solution.size == 1 && completeSounds) {
-            if (!cancelLastClick) playTerminalSound()
-            playCompleteSound()
-        } else playTerminalSound()
+        if (!shouldReplaceSounds()) return
+        clickSlot(event.slot)
     }
 
     @SubscribeEvent
     fun onTermComplete(event: TerminalSolvedEvent) {
-        if (event.playerName == mc.thePlayer?.name && completeSounds) return
-        mc.addScheduledTask { mc.thePlayer.playSound("note.pling", 8f, 4f) }
+        if (shouldReplaceSounds() && event.playerName != mc.thePlayer?.name || (!completeSounds && !clickSounds)) mc.addScheduledTask { mc.thePlayer.playSound("note.pling", 8f, 4f) }
+        else if (shouldReplaceSounds() && completeSounds && !clickSounds) mc.addScheduledTask { playCompleteSound() }
     }
 
     init {
-        onMessage("The gate has been destroyed!", false) { mc.addScheduledTask { mc.thePlayer.playSound("note.pling", 8f, 4f) } }
+        onMessage("The gate has been destroyed!", false, { enabled && shouldReplaceSounds() }) { mc.addScheduledTask { mc.thePlayer.playSound("note.pling", 8f, 4f) } }
 
-        onMessage("The Core entrance is opening!", false) { mc.addScheduledTask { mc.thePlayer.playSound("note.pling", 8f, 4f) } }
+        onMessage("The Core entrance is opening!", false, { enabled && shouldReplaceSounds() }) { mc.addScheduledTask { mc.thePlayer.playSound("note.pling", 8f, 4f) } }
+    }
+
+    private fun clickSlot(slot: Int) {
+        if (
+            (currentTerm != TerminalTypes.ORDER && slot !in TerminalSolver.solution) ||
+            (currentTerm == TerminalTypes.ORDER && slot != TerminalSolver.solution.first()) ||
+            (currentTerm == TerminalTypes.MELODY && slot !in arrayOf(43, 34, 25, 16))
+        ) return
+        if ((TerminalSolver.solution.size == 1 || currentTerm == TerminalTypes.MELODY && slot == 43) && completeSounds) {
+            if (!cancelLastClick) playTerminalSound()
+            playCompleteSound()
+        } else playTerminalSound()
     }
 
     fun playCompleteSound() {
@@ -107,5 +103,9 @@ object TerminalSounds : Module(
         val sound = if (sound == defaultSounds.size - 1) customSound else defaultSounds[sound]
         mc.addScheduledTask { PlayerUtils.playLoudSound(sound, clickVolume, clickPitch) }
         lastPlayed = System.currentTimeMillis()
+    }
+
+    private fun shouldReplaceSounds(): Boolean {
+        return (currentTerm != TerminalTypes.NONE && clickSounds)
     }
 }
