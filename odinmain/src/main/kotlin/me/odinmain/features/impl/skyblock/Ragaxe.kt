c2efef26f14633b0827cd9d5fb664fee5ade61e6
odtheking <ohad.dotan@gmail.com>
2024-10-14 15:44:36 +0300

    Made Ragaxe sound based

--- odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/Ragaxe.kt
+++ odinmain/src/main/kotlin/me/odinmain/features/impl/skyblock/Ragaxe.kt
@@ -4,12 +4,12 @@ import me.odinmain.features.Category
 import me.odinmain.features.Module
 import me.odinmain.features.settings.Setting.Companion.withDependency
 import me.odinmain.features.settings.impl.BooleanSetting
-import me.odinmain.utils.runIn
 import me.odinmain.utils.skyblock.PlayerUtils
 import me.odinmain.utils.skyblock.heldItem
-import me.odinmain.utils.skyblock.strength
 import me.odinmain.utils.skyblock.modMessage
 import me.odinmain.utils.skyblock.partyMessage
+import me.odinmain.utils.skyblock.strength
+import net.minecraft.network.play.server.S29PacketSoundEffect
 
 object Ragaxe : Module(
     name = "Rag Axe",
@@ -21,39 +21,18 @@ object Ragaxe : Module(
     private val strengthGainedMessage by BooleanSetting("Strength Gained", true, description = "Sends a mod message which will notify of strength gained from rag axe after casting")
     private val announceStrengthGained by BooleanSetting("Send to party", true, description = "Sends party message of strength gained after casting").withDependency { strengthGainedMessage }
 
-    var cancelled = false
-    var casting = false
-
     init {
-        onMessage(Regex("^.+CASTING IN 3s(.+)?\$"), { alert && enabled && !casting }) {
-            casting = true
-            runIn(62) {
-
-              if (cancelled) {
-                    cancelled = false
-                    casting = false
-                    return@runIn
-                }
-
-                PlayerUtils.alert("§aCasted Rag Axe")
-                val strengthGain = ((heldItem?.strength ?: 0) * 1.5).toInt()
-                if (strengthGainedMessage) modMessage("Gained strength: $strengthGain")
-                if (announceStrengthGained) partyMessage("Gained strength from rag axe: $strengthGain")
-                casting = false
-            }
+        onMessage(Regex("Ragnarock was cancelled due to (?:being hit|taking damage)!"),) {
+            if (alertCancelled) PlayerUtils.alert("§cRag Axe Cancelled")
         }
 
-        onMessage(Regex("Ragnarock was cancelled due to (?:being hit|taking damage)!"), { alertCancelled && enabled }) {
-            PlayerUtils.alert("§cRag Axe Cancelled")
-            cancelled = true
+        onPacket(S29PacketSoundEffect::class.java) {
+            if (it.soundName != "mob.wolf.howl" || it.pitch != 1.4920635f) return@onPacket
+            if (alert) PlayerUtils.alert("§aCasted Rag Axe")
+            val strengthGain = ((heldItem?.strength ?: 0) * 1.5).toInt()
+            if (strengthGainedMessage) modMessage("Gained strength: $strengthGain")
+            if (announceStrengthGained) partyMessage("Gained strength from rag axe: $strengthGain")
 
-            casting = false
-        }
-
-        onWorldLoad {
-            cancelled = false
-            casting = false
         }
     }
-
 }
