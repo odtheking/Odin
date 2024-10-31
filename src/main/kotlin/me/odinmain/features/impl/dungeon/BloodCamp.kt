749d7f807d62891b096ecf6ea72276ef47c1dcb2
SubAt0mic <pokemonjunkbox@gmail.com>
2024-10-31 15:20:41 -0400

    fix watcher health displaying null

--- src/main/kotlin/me/odinmain/features/impl/dungeon/BloodCamp.kt
+++ src/main/kotlin/me/odinmain/features/impl/dungeon/BloodCamp.kt
@@ -78,7 +78,7 @@ object BloodCamp : Module(
         if (!inDungeons || inBoss || event.type != RenderGameOverlayEvent.ElementType.BOSSHEALTH
             || !watcherBar || BossStatus.bossName.noControlCodes != "The Watcher") return
         val amount = 12 + DungeonUtils.floor.floorNumber
-        BossStatus.bossName += BossStatus.healthScale.takeIf { it >= 0.05 }?.let { " ${(amount * it).roundToInt()}/$amount" }
+        BossStatus.bossName += BossStatus.healthScale.takeIf { it >= 0.05 }?.let { " ${(amount * it).roundToInt()}/$amount" } ?: ""
     }
 
     private var currentTickTime: Long = 0
