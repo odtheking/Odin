9406a2c9f2e70386bf020d2cd0de38c4f55309f9
xupie <81492239+Xupie@users.noreply.github.com>
2024-10-31 22:04:13 +0200

    changes

--- src/main/kotlin/me/odinmain/features/impl/nether/KuudraReminders.kt
+++ src/main/kotlin/me/odinmain/features/impl/nether/KuudraReminders.kt
@@ -25,28 +25,27 @@ object KuudraReminders : Module(
     private val onlyKuudra by BooleanSetting("Notify in Kuudra Only", true, description = "Notify of mana drain only when in Kuudra.").withDependency { manaDrain }
 
     init {
-        onMessage("WARNING: You do not have a key for this tier in your inventory, you will not be able to claim rewards.", false, { keyReminder && enabled }) {
+        onMessage(Regex("WARNING: You do not have a key for this tier in your inventory, you will not be able to claim rewards."), { keyReminder && enabled }) {
             PlayerUtils.alert("No key in inventory", displayText = displayText, playSound = playSound)
         }
 
-        onMessage("[NPC] Elle: Okay adventurers, I will go and fish up Kuudra!", false, { buyUpgrades && enabled }) {
+        onMessage(Regex("\\[NPC] Elle: Okay adventurers, I will go and fish up Kuudra!"), { buyUpgrades && enabled }) {
             PlayerUtils.alert("Buy Upgrades", displayText = displayText, playSound = playSound)
         }
 
-        onMessage("[NPC] Elle: Not again!", false, { pickUpSupplies && enabled }) {
+        onMessage(Regex("\\[NPC] Elle: Not again!"), { pickUpSupplies && enabled }) {
             PlayerUtils.alert("Pick up supplies", displayText = displayText, playSound = playSound)
         }
 
-        onMessage("[NPC] Elle: It's time to build the Ballista again! Cover me!", false, { buildBallista && enabled }) {
+        onMessage(Regex("\\[NPC] Elle: It's time to build the Ballista again! Cover me!"), { buildBallista && enabled }) {
             PlayerUtils.alert("Build Ballista", displayText = displayText, playSound = playSound)
         }
 
-        onMessage("Your Fresh Tools Perk bonus doubles your building speed for the next 10 seconds!", false, { freshTools && enabled }) {
+        onMessage(Regex("Your Fresh Tools Perk bonus doubles your building speed for the next 10 seconds!"), { freshTools && enabled }) {
             PlayerUtils.alert("Fresh Tools", displayText = displayText, playSound = playSound)
         }
 
-        onMessage(Regex("Used Extreme Focus! \\((\\d+) Mana\\)"), { manaDrain && enabled}) {
-            if (onlyKuudra && !KuudraUtils.inKuudra) return@onMessage
+        onMessage(Regex("Used Extreme Focus! \\((\\d+) Mana\\)"), { manaDrain && enabled && (!onlyKuudra || (onlyKuudra && KuudraUtils.inKuudra))}) {
             val mana = Regex("Used Extreme Focus! \\((\\d+) Mana\\)").find(it)?.groupValues?.get(1)?.toIntOrNull() ?: return@onMessage
             val amount = mc.theWorld?.playerEntities?.filter { entity -> entity.isOtherPlayer() && entity.getDistanceSqToEntity(mc.thePlayer) < 49 }?.size?.coerceAtLeast(0)
             partyMessage("Used $mana mana on $amount people.")
