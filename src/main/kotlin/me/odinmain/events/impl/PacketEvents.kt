c4de77381a53c5f63809ea7419dc1cc7d6259a23
odtheking <ohad.dotan@gmail.com>
2024-11-22 12:03:45 +0200

    Made PacketEvent cancellable

--- src/main/kotlin/me/odinmain/events/impl/PacketEvents.kt
+++ src/main/kotlin/me/odinmain/events/impl/PacketEvents.kt
@@ -1,9 +1,14 @@
 package me.odinmain.events.impl
 
 import net.minecraft.network.Packet
+import net.minecraftforge.fml.common.eventhandler.Cancelable
 import net.minecraftforge.fml.common.eventhandler.Event
 
 open class PacketEvent(val packet: Packet<*>) : Event() {
+
+    @Cancelable
     class Receive(packet: Packet<*>) : PacketEvent(packet)
+
+    @Cancelable
     class Send(packet: Packet<*>) : PacketEvent(packet)
 }
