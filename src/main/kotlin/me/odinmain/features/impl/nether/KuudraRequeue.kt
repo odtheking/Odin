7b2e3a375f558ad9eff7286ce5ec0999b361f0c4
Jordyrat <j.ratcliffe2003@outlook.com>
2025-02-16 15:50:09 +0000

    Added additional Regex for disablePartyLeave to catch you/others leaving/disabanding the party

--- src/main/kotlin/me/odinmain/features/impl/nether/KuudraRequeue.kt
+++ src/main/kotlin/me/odinmain/features/impl/nether/KuudraRequeue.kt
@@ -31,7 +31,19 @@ object KuudraRequeue : Module(
             }
         }
 
-        onMessage(Regex("(\\[.+])? ?(.{1,16}) has (left|been removed from) the party.")) {
+        onMessage(Regex("(\\[.+])? ?(.{1,16}) has (left|been removed from|disbanded) the party.")) {
+            if (disablePartyLeave) disableRequeue = true
+        }
+        onMessage(Regex("You have been kicked from the party by (\\[.+])? ?(.{1,16})")) {
+            if (disablePartyLeave) disableRequeue = true
+        }
+        onMessage(Regex("You left the party.")) {
+            if (disablePartyLeave) disableRequeue = true
+        }
+        onMessage(Regex("The party was transferred to (\\[.+])? ?(.{1,16}) because (\\[.+])? ?(.{1,16}) left")) {
+            if (disablePartyLeave) disableRequeue = true
+        }
+        onMessage(Regex("The party was disbanded because all invites expired and the party was empty.")) {
             if (disablePartyLeave) disableRequeue = true
         }
     }
