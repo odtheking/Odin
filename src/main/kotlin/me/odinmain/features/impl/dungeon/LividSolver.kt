e24c71cd4091c51156e97839753688968d23a630
FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>
2025-04-01 10:37:33 +0800

    1.5. im dumb
    Signed-off-by: FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>

--- src/main/kotlin/me/odinmain/features/impl/dungeon/LividSolver.kt
+++ src/main/kotlin/me/odinmain/features/impl/dungeon/LividSolver.kt
@@ -6,6 +6,7 @@ import me.odinmain.events.impl.PostEntityMetadata
 import me.odinmain.features.Category
 import me.odinmain.features.Module
 import me.odinmain.features.settings.Setting.Companion.withDependency
+import me.odinmain.features.settings.impl.BooleanSetting
 import me.odinmain.features.settings.impl.NumberSetting
 import me.odinmain.features.settings.impl.SelectorSetting
 import me.odinmain.utils.render.Color
@@ -27,7 +28,7 @@ object LividSolver : Module(
     private val mode by SelectorSetting("Mode", HighlightRenderer.HIGHLIGHT_MODE_DEFAULT, HighlightRenderer.highlightModeList, description = HighlightRenderer.HIGHLIGHT_MODE_DESCRIPTION)
     private val thickness by NumberSetting("Line Width", 1f, .1f, 4f, .1f, description = "The line width of Outline / Boxes/ 2D Boxes.").withDependency { mode != HighlightRenderer.HighlightType.Overlay.ordinal }
     private val style by SelectorSetting("Style", Renderer.DEFAULT_STYLE, Renderer.styles, description = Renderer.STYLE_DESCRIPTION).withDependency { mode == HighlightRenderer.HighlightType.Boxes.ordinal }
-    private val showTracer by SelectorSetting("Show Tracer", false, description = "Shows a tracer to the highlighted Livid.")
+    private val showTracer by BooleanSetting("Show Tracer", false, description = "Shows a tracer to the highlighted Livid.")
 
     private val woolLocation = BlockPos(5, 108, 43)
     private var currentLivid = Livid.HOCKEY
