8b000cd57b3c1b5633f3c99c50fb0862ed08eb2a
odtheking <odtheking1@gmail.com>
2025-04-17 07:52:09 +0300

    Reverted Blue WitherDragonEnum to Aqua

--- src/main/kotlin/me/odinmain/features/impl/floor7/WitherDragonEnum.kt
+++ src/main/kotlin/me/odinmain/features/impl/floor7/WitherDragonEnum.kt
@@ -43,13 +43,13 @@ enum class WitherDragonsEnum (
 ) {
     Red(   Vec3(27.0, 14.0, 59.0), AxisAlignedBB(14.5, 13.0, 45.5, 39.5, 28.0, 70.5),   'c', Colors.MINECRAFT_RED,   24.0..30.0, 56.0..62.0,  skipKillTime = 50),
 
-    Orange(Vec3(85.0, 14.0, 56.0), AxisAlignedBB(72.0, 8.0,  47.0, 102.0,28.0, 77.0),   '6', Colors.MINECRAFT_GOLD,82.0..88.0, 53.0..59.0,  skipKillTime = 62),
+    Orange(Vec3(85.0, 14.0, 56.0), AxisAlignedBB(72.0, 8.0,  47.0, 102.0,28.0, 77.0),   '6', Colors.MINECRAFT_GOLD,  82.0..88.0, 53.0..59.0,  skipKillTime = 62),
 
     Green( Vec3(27.0, 14.0, 94.0), AxisAlignedBB(7.0,  8.0,  80.0, 37.0, 28.0, 110.0),  'a', Colors.MINECRAFT_GREEN, 23.0..29.0, 91.0..97.0,  skipKillTime = 52),
 
-    Blue(  Vec3(84.0, 14.0, 94.0), AxisAlignedBB(71.5, 16.0, 82.5, 96.5, 26.0, 107.5),  'b', Colors.MINECRAFT_BLUE,  82.0..88.0, 91.0..97.0,  skipKillTime = 39),
+    Blue(  Vec3(84.0, 14.0, 94.0), AxisAlignedBB(71.5, 16.0, 82.5, 96.5, 26.0, 107.5),  'b', Colors.MINECRAFT_AQUA,  82.0..88.0, 91.0..97.0,  skipKillTime = 39),
 
-    Purple(Vec3(56.0, 14.0, 125.0), AxisAlignedBB(45.5, 13.0, 113.5,68.5, 23.0, 136.5), '5', Colors.MINECRAFT_DARK_PURPLE,53.0..59.0, 122.0..128.0, skipKillTime = 38),
+    Purple(Vec3(56.0, 14.0, 125.0), AxisAlignedBB(45.5, 13.0, 113.5,68.5, 23.0, 136.5), '5', Colors.MINECRAFT_DARK_PURPLE, 53.0..59.0, 122.0..128.0, skipKillTime = 38),
 
     None(  Vec3(0.0, 0.0, 0.0),     AxisAlignedBB(0.0, 0.0, 0.0, 0.0, 0.0, 0.0),        'f', Colors.WHITE, 0.0..0.0, 0.0..0.0);
 
