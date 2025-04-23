50208f84430b5131024ecdd40cd0f3c9f4f16ea5
MCSuchter <49833081+MCSuchter@users.noreply.github.com>
2025-04-23 20:20:34 +0200

    make leaporder less scuffed

--- src/main/kotlin/me/odinmain/commands/impl/OdinCommand.kt
+++ src/main/kotlin/me/odinmain/commands/impl/OdinCommand.kt
@@ -122,7 +122,7 @@ val mainCommand = Commodore("od", "odin") {
     runs { tier: KuudraTier -> sendCommand("joininstance ${tier.instance()}") }
 
     literal("leaporder").runs { player1: String?, player2: String?, player3: String?, player4: String? ->
-        val players = listOfNotNull(player1, player2, player3, player4)
+        val players = listOfNotNull(player1.lowercase(), player2.lowercase(), player3.lowercase(), player4.lowercase())
         DungeonUtils.customLeapOrder = players
         modMessage("§aCustom leap order set to: §f${players.joinToString(", ")}")
     }
