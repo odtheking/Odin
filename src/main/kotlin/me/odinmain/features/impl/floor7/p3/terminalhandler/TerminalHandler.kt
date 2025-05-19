a6c7336793bf98640457875cbbf572996b3242d7
Jordyrat <j.ratcliffe2003@outlook.com>
2025-05-19 20:05:56 +0100

    Jordyrat Stupid

--- src/main/kotlin/me/odinmain/features/impl/floor7/p3/terminalhandler/TerminalHandler.kt
+++ src/main/kotlin/me/odinmain/features/impl/floor7/p3/terminalhandler/TerminalHandler.kt
@@ -51,7 +51,8 @@ open class TerminalHandler(val type: TerminalTypes, val timeOpened: Long = Syste
     }
 
     fun canClick(slotIndex: Int, button: Int, needed: Int = solution.count { it == slotIndex }): Boolean = when {
-        type == TerminalTypes.MELODY -> slotIndex.equalsOneOf(16, 25, 34, 43) && TerminalSolver.melodyCorrect
+        type == TerminalTypes.MELODY && TerminalSolver.renderType == 3 && TerminalSolver.melodyCorrect -> false
+        type == TerminalTypes.MELODY -> slotIndex.equalsOneOf(16, 25, 34, 43)
         slotIndex !in solution -> false
         type == TerminalTypes.NUMBERS && slotIndex != solution.firstOrNull() -> false
         type == TerminalTypes.RUBIX && ((needed < 3 && button == 1) || (needed >= 3 && button != 1)) -> false
