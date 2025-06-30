fc3696fc361689810327728222b427fc422c46c4
FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>
2025-06-30 19:24:38 +0800

    Add isInSkyblock check in CommandKeybinds
    Signed-off-by: FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>

--- src/main/kotlin/me/odinmain/features/impl/skyblock/CommandKeybinds.kt
+++ src/main/kotlin/me/odinmain/features/impl/skyblock/CommandKeybinds.kt
@@ -2,6 +2,7 @@ package me.odinmain.features.impl.skyblock
 
 import me.odinmain.features.Module
 import me.odinmain.features.settings.impl.KeybindSetting
+import me.odinmain.utils.skyblock.LocationUtils.isInSkyblock
 import me.odinmain.utils.skyblock.sendCommand
 import org.lwjgl.input.Keyboard
 
@@ -11,23 +12,23 @@ object CommandKeybinds : Module(
     key = null
 ) {
     private val pets by KeybindSetting("Pets", Keyboard.KEY_NONE, description = "Opens the pets menu.").onPress {
-        if (!enabled) return@onPress
+        if (!enabled || !isInSkyblock) return@onPress
         sendCommand("pets")
     }
     private val storage by KeybindSetting("Storage", Keyboard.KEY_NONE, description = "Opens the storage menu.").onPress {
-        if (!enabled) return@onPress
+        if (!enabled || !isInSkyblock) return@onPress
         sendCommand("storage")
     }
     private val wardrobe by KeybindSetting("Wardrobe", Keyboard.KEY_NONE, description = "Opens the wardrobe menu.").onPress {
-        if (!enabled) return@onPress
+        if (!enabled || !isInSkyblock) return@onPress
         sendCommand("wardrobe")
     }
     private val equipment by KeybindSetting("Equipment", Keyboard.KEY_NONE, description = "Opens the equipment menu.").onPress {
-        if (!enabled) return@onPress
+        if (!enabled || !isInSkyblock) return@onPress
         sendCommand("equipment")
     }
     private val dhub by KeybindSetting("Dungeon Hub", Keyboard.KEY_NONE, description = "Warps to the dungeon hub.").onPress {
-        if (!enabled) return@onPress
+        if (!enabled || !isInSkyblock) return@onPress
         sendCommand("warp dungeon_hub")
     }
 }
