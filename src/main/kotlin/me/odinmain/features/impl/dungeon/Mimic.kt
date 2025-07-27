91f0f65c7d57ce5df0276cd7cc71a824f1a23007
SubAt0mic <pokemonjunkbox@gmail.com>
2025-07-26 23:31:16 -0400

    uhhh i forgot to make it toggleable

--- src/main/kotlin/me/odinmain/features/impl/dungeon/Mimic.kt
+++ src/main/kotlin/me/odinmain/features/impl/dungeon/Mimic.kt
@@ -33,10 +33,11 @@ object Mimic : Module(
     private val color by ColorSetting("Color", Colors.MINECRAFT_RED.withAlpha(0.5f), allowAlpha = true, desc = "The color of the box.").withDependency { mimicBox }
     private val lineWidth by NumberSetting("Line Width", 2f, 0.1f, 10f, 0.1f, desc = "The width of the box's lines.").withDependency { mimicBox }
 
-    private val princeMessageToggle by BooleanSetting("Toggle Prince Message", false, desc = "Toggles the prince killed message. Only enable this with 100% extra score chance from princes.")
-    val princeMessage by StringSetting("Prince Message", "Prince Killed!", 128, desc = "Message sent when prince is detected as killed.").withDependency { princeMessageToggle }
-    val range by NumberSetting("Range", 10f, 2f, 30f, 1f, desc = "The range at which princes will be detected.", unit = " Blocks").withDependency { princeMessageToggle }
-    private val princeReset by ActionSetting("Prince Killed", desc = "Sends Prince killed message in party chat.") { princeKilled() }
+    private val princeDetection by BooleanSetting("Prince Detection", default = false, desc = "Enables prince detection. Only enable if you have 100% extra score chance from princes.")
+    private val princeMessageToggle by BooleanSetting("Toggle Prince Message", false, desc = "Toggles the prince killed message.").withDependency { princeDetection }
+    val princeMessage by StringSetting("Prince Message", "Prince Killed!", 128, desc = "Message sent when prince is detected as killed.").withDependency { princeDetection && princeMessageToggle }
+    val range by NumberSetting("Range", 10f, 2f, 30f, 1f, desc = "The range at which princes will be detected.", unit = " Blocks").withDependency { princeDetection && princeMessageToggle }
+    private val princeReset by ActionSetting("Prince Killed", desc = "Sends Prince killed message in party chat.") { princeKilled() }.withDependency { princeDetection }
 
     private const val MIMIC_TEXTURE = "ewogICJ0aW1lc3RhbXAiIDogMTY3Mjc2NTM1NTU0MCwKICAicHJvZmlsZUlkIiA6ICJhNWVmNzE3YWI0MjA0MTQ4ODlhOTI5ZDA5OTA0MzcwMyIsCiAgInByb2ZpbGVOYW1lIiA6ICJXaW5zdHJlYWtlcnoiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTE5YzEyNTQzYmM3NzkyNjA1ZWY2OGUxZjg3NDlhZThmMmEzODFkOTA4NWQ0ZDRiNzgwYmExMjgyZDM1OTdhMCIsCiAgICAgICJtZXRhZGF0YSIgOiB7CiAgICAgICAgIm1vZGVsIiA6ICJzbGltIgogICAgICB9CiAgICB9CiAgfQp9"
     private const val PRINCE_TEXTURE = "ewogICJ0aW1lc3RhbXAiIDogMTU5MDE3Njk2NjUxNywKICAicHJvZmlsZUlkIiA6ICJhMmY4MzQ1OTVjODk0YTI3YWRkMzA0OTcxNmNhOTEwYyIsCiAgInByb2ZpbGVOYW1lIiA6ICJiUHVuY2giLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDk5OTEyM2NmMGE0MTQ3N2Y3MDZmYzZhNTA4OTE0NjNlOGNiNTBkY2JkZDJjODFjNjUyZmNlZjZmZGJkZTIxYyIKICAgIH0KICB9Cn0="
@@ -50,7 +51,7 @@ object Mimic : Module(
     fun onEntityDeath(event: LivingDeathEvent) = with(event.entity) {
         if (!DungeonUtils.inDungeons) return@with
         when {
-            this is EntityOtherPlayerMP && getEntityTexture(this) == PRINCE_TEXTURE && mc.thePlayer.getDistanceToEntity(this) <= range -> princeKilled()
+            princeDetection && this is EntityOtherPlayerMP && getEntityTexture(this) == PRINCE_TEXTURE && mc.thePlayer.getDistanceToEntity(this) <= range -> princeKilled()
             this is EntityZombie && isChild && (0..3).all { getCurrentArmor(it) == null } -> mimicKilled()
         }
     }
