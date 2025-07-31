d651d064daea0ef14403b224eac5cb68864b1c1d
FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>
2025-07-31 23:53:16 +0800

    Dropdowns
    Signed-off-by: FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>

--- src/main/kotlin/me/odinmain/features/impl/dungeon/Mimic.kt
+++ src/main/kotlin/me/odinmain/features/impl/dungeon/Mimic.kt
@@ -24,21 +24,24 @@ object Mimic : Module(
     name = "Mimic",
     description = "Highlights and announces mimic kills in dungeons."
 ) {
-    private val mimicBox by BooleanSetting("Mimic Box", true, desc = "Draws a box around the mimic chest.")
-    private val style by SelectorSetting("Style", Renderer.DEFAULT_STYLE, Renderer.styles, desc = Renderer.STYLE_DESCRIPTION).withDependency { mimicBox }
-    private val color by ColorSetting("Color", Colors.MINECRAFT_RED.withAlpha(0.5f), allowAlpha = true, desc = "The color of the box.").withDependency { mimicBox }
-    private val lineWidth by NumberSetting("Line Width", 2f, 0.1f, 10f, 0.1f, desc = "The width of the box's lines.").withDependency { mimicBox }
+    private val mimicBoxDropdown by DropdownSetting("Mimic Box Dropdown")
+    private val mimicBox by BooleanSetting("Mimic Box", true, desc = "Draws a box around the mimic chest.").withDependency { mimicBoxDropdown }
+    private val style by SelectorSetting("Style", Renderer.DEFAULT_STYLE, Renderer.styles, desc = Renderer.STYLE_DESCRIPTION).withDependency { mimicBoxDropdown && mimicBox }
+    private val color by ColorSetting("Color", Colors.MINECRAFT_RED.withAlpha(0.5f), allowAlpha = true, desc = "The color of the box.").withDependency { mimicBoxDropdown && mimicBox }
+    private val lineWidth by NumberSetting("Line Width", 2f, 0.1f, 10f, 0.1f, desc = "The width of the box's lines.").withDependency { mimicBoxDropdown && mimicBox }
 
-    private val mimicMessageToggle by BooleanSetting("Toggle Mimic Message", true, desc = "Toggles the mimic killed message.")
-    val mimicMessage by StringSetting("Mimic Message", "Mimic Killed!", 128, desc = "Message sent when mimic is detected as killed.").withDependency { mimicMessageToggle }
-    private val mimicReset by ActionSetting("Mimic Killed", desc = "Sends Mimic killed message in party chat.") { mimicKilled() }
+    private val mimicDropdown by DropdownSetting("Mimic Dropdown")
+    private val mimicMessageToggle by BooleanSetting("Toggle Mimic Message", true, desc = "Toggles the mimic killed message.").withDependency { mimicDropdown }
+    val mimicMessage by StringSetting("Mimic Message", "Mimic Killed!", 128, desc = "Message sent when mimic is detected as killed.").withDependency { mimicDropdown && mimicMessageToggle }
+    private val mimicReset by ActionSetting("Mimic Killed", desc = "Sends Mimic killed message in party chat.") { mimicKilled() }.withDependency{ mimicDropdown }
 
-    private val princeDetection by BooleanSetting("Max Reborn Attribute", false, desc = "Enables prince detection. Only enable if you have 100% extra score chance from princes.")
-    private val princeRange by NumberSetting("Detection Range", 10f, 2f, 30f, 1f, desc = "The range at which princes will be detected.", unit = " Blocks").withDependency { princeDetection }
-    private val princeStrict by BooleanSetting("Strict Mode", true, desc = "Don't count when a teammate is closer to the prince than you are.").withDependency { princeDetection }
-    private val princeMessageToggle by BooleanSetting("Toggle Prince Message", false, desc = "Toggles the prince killed message.").withDependency { princeDetection }
-    val princeMessage by StringSetting("Prince Message", "Prince Killed!", 128, desc = "Message sent when prince is detected as killed.").withDependency { princeDetection && princeMessageToggle }
-    private val princeReset by ActionSetting("Prince Killed", desc = "Sends Prince killed message in party chat.") { princeKilled() }.withDependency { princeDetection }
+    private val princeDropdown by DropdownSetting("Prince Dropdown")
+    private val princeDetection by BooleanSetting("Max Reborn Attribute", false, desc = "Enables prince detection. Only enable if you have 100% extra score chance from princes.").withDependency { princeDropdown }
+    private val princeRange by NumberSetting("Detection Range", 10f, 2f, 30f, 1f, desc = "The range at which princes will be detected.", unit = " Blocks").withDependency { princeDropdown && princeDetection }
+    private val princeStrict by BooleanSetting("Strict Mode", true, desc = "Don't count when a teammate is closer to the prince than you are.").withDependency { princeDropdown && princeDetection }
+    private val princeMessageToggle by BooleanSetting("Toggle Prince Message", false, desc = "Toggles the prince killed message.").withDependency { princeDropdown && princeDetection }
+    val princeMessage by StringSetting("Prince Message", "Prince Killed!", 128, desc = "Message sent when prince is detected as killed.").withDependency { princeDropdown && princeDetection && princeMessageToggle }
+    private val princeReset by ActionSetting("Prince Killed", desc = "Sends Prince killed message in party chat.") { princeKilled() }.withDependency { princeDropdown && princeDetection }
 
     private const val MIMIC_TEXTURE = "ewogICJ0aW1lc3RhbXAiIDogMTY3Mjc2NTM1NTU0MCwKICAicHJvZmlsZUlkIiA6ICJhNWVmNzE3YWI0MjA0MTQ4ODlhOTI5ZDA5OTA0MzcwMyIsCiAgInByb2ZpbGVOYW1lIiA6ICJXaW5zdHJlYWtlcnoiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTE5YzEyNTQzYmM3NzkyNjA1ZWY2OGUxZjg3NDlhZThmMmEzODFkOTA4NWQ0ZDRiNzgwYmExMjgyZDM1OTdhMCIsCiAgICAgICJtZXRhZGF0YSIgOiB7CiAgICAgICAgIm1vZGVsIiA6ICJzbGltIgogICAgICB9CiAgICB9CiAgfQp9"
     private const val PRINCE_TEXTURE = "ewogICJ0aW1lc3RhbXAiIDogMTU5MDE3Njk2NjUxNywKICAicHJvZmlsZUlkIiA6ICJhMmY4MzQ1OTVjODk0YTI3YWRkMzA0OTcxNmNhOTEwYyIsCiAgInByb2ZpbGVOYW1lIiA6ICJiUHVuY2giLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDk5OTEyM2NmMGE0MTQ3N2Y3MDZmYzZhNTA4OTE0NjNlOGNiNTBkY2JkZDJjODFjNjUyZmNlZjZmZGJkZTIxYyIKICAgIH0KICB9Cn0="
