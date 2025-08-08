1b03822f9e6ef37f7d9028aacfe321f6de8f65a7
FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>
2025-08-08 14:56:40 +0800

    revert ping :<
    Signed-off-by: FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>

--- src/main/kotlin/me/odinmain/utils/ServerUtils.kt
+++ src/main/kotlin/me/odinmain/utils/ServerUtils.kt
@@ -4,9 +4,10 @@ import me.odinmain.OdinMain.mc
 import me.odinmain.events.impl.PacketEvent
 import me.odinmain.utils.clock.Executor
 import me.odinmain.utils.clock.Executor.Companion.register
-import me.odinmain.utils.skyblock.sendCommand
-import net.minecraft.network.play.server.S02PacketChat
+import me.odinmain.utils.skyblock.LocationUtils.isOnHypixel
+import net.minecraft.network.play.client.C16PacketClientStatus
 import net.minecraft.network.play.server.S03PacketTimeUpdate
+import net.minecraft.network.play.server.S37PacketStatistics
 import net.minecraftforge.event.world.WorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -17,28 +18,24 @@ object ServerUtils {
     var averagePing = 0f
         private set
 
-    private val unknownCommandRegex = Regex("^/?Unknown command\\. Type \"/?help\" for help\\. \\('odingetpingcommand-----'\\)$")
     private var pingStartTime = 0L
     private var isPinging = false
     private var prevTime = 0L
 
     init {
         Executor(2000, "ServerUtils") {
-            if (mc.isSingleplayer) return@Executor
+            if (!isOnHypixel) return@Executor
             pingStartTime = System.nanoTime()
             isPinging = true
-
-            sendCommand("odingetpingcommand-----")
+            mc.netHandler?.addToSendQueue(C16PacketClientStatus(C16PacketClientStatus.EnumState.REQUEST_STATS))
         }.register()
     }
 
     @SubscribeEvent
     fun onPacket(event: PacketEvent.Receive) {
         when (event.packet) {
-            is S02PacketChat -> {
-                if (event.packet.chatComponent?.unformattedText?.noControlCodes?.let { unknownCommandRegex.matches(it) } == false) return
+            is S37PacketStatistics -> {
                 averagePing = (System.nanoTime() - pingStartTime) / 1e6f
-                event.isCanceled = true
                 isPinging = false
             }
 
