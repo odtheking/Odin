fd5ee1f189d4a42e97adcd9f0f9fd22ee942ebf4
Tjiba <tjiba@tjiba.fr>
2026-04-01 08:43:44 +0200

    + No item Place - Module . to prevent placing for example . flowers item and superboom tnt/infinitibomb.
    Can still place player heads, so ok in dungeon for rooms with the red head.
    
    Only in skyblock

---
+++ src/main/kotlin/com/odtheking/odin/features/impl/skyblock/NoItemPlace.kt
@@ -0,0 +1,27 @@
+package com.odtheking.odin.features.impl.skyblock
+
+import com.odtheking.odin.events.BlockInteractEvent
+import com.odtheking.odin.events.core.on
+import com.odtheking.odin.events.core.onSend
+import com.odtheking.odin.features.Module
+import com.odtheking.odin.utils.skyblock.LocationUtils
+import net.minecraft.network.protocol.game.ServerboundUseItemOnPacket
+import net.minecraft.world.item.BlockItem
+import net.minecraft.world.level.block.PlayerHeadBlock
+
+object NoItemPlace : Module(
+    name = "No Item Place",
+    description = "Prevents placing block items while playing SkyBlock."
+) {
+    init {
+        on<BlockInteractEvent> {
+            if (!LocationUtils.isInSkyblock || !enabled) return@on
+
+            val heldItem = mc.player?.mainHandItem?.item as? BlockItem ?: return@on
+            if (heldItem.block is PlayerHeadBlock) return@on
+
+            cancel()
+        }
+    }
+}
+
