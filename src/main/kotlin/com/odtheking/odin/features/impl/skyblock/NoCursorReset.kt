849ff64227b7d36aea4763f55b4c2f786b2d1966
odtheking <odtheking1@gmail.com>
2026-04-07 14:48:35 +0300

    Added option for the unhook timeout in NoCursorReset Made LividSolver not wait for blindness duration

--- src/main/kotlin/com/odtheking/odin/features/impl/skyblock/NoCursorReset.kt
+++ src/main/kotlin/com/odtheking/odin/features/impl/skyblock/NoCursorReset.kt
@@ -1,5 +1,6 @@
 package com.odtheking.odin.features.impl.skyblock
 
+import com.odtheking.odin.clickgui.settings.impl.NumberSetting
 import com.odtheking.odin.events.TickEvent
 import com.odtheking.odin.events.core.on
 import com.odtheking.odin.features.Module
@@ -8,6 +9,8 @@ object NoCursorReset : Module(
     name = "No Cursor Reset",
     description = "Prevents the cursor from being reset when opening a GUI."
 ) {
+    private val unhookTimeout by NumberSetting("Unhook Timeout", 150, 0, 1000, 10, "The amount of milliseconds after opening a GUI to prevent the cursor from being reset.", unit = "ms")
+
     private var clock = System.currentTimeMillis()
     private var wasNotNull = false
 
@@ -25,5 +28,5 @@ object NoCursorReset : Module(
 
     @JvmStatic
     fun shouldHookMouse(): Boolean =
-        System.currentTimeMillis() - clock < 150 && enabled
+        System.currentTimeMillis() - clock < unhookTimeout && enabled
 }
