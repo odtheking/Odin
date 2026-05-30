02f751c9e89c8cbf12a67fdd560f2aa7ed4fd4ac
odtheking <odtheking1@gmail.com>
2026-05-30 09:50:54 +0300

    Removed string manipulation

--- src/main/kotlin/com/odtheking/odin/utils/skyblock/dungeon/terminals/terminalhandler/SelectAllHandler.kt
+++ src/main/kotlin/com/odtheking/odin/utils/skyblock/dungeon/terminals/terminalhandler/SelectAllHandler.kt
@@ -10,47 +10,24 @@ import net.minecraft.world.item.Items
 
 class SelectAllHandler(private val color: DyeColor): TerminalHandler(TerminalTypes.SELECT) {
 
-    private fun fixName(name: String): String {
-        val replacements = mapOf(
-            "light gray" to "silver",
-            "wool" to "white",
-            "bone" to "white",
-            "ink" to "black",
-            "lapis" to "blue",
-            "cocoa" to "brown",
-            "dandelion" to "yellow",
-            "rose" to "red",
-            "poppy" to "red",
-            "cactus" to "green"
-        )
-
-        var fixed = name.lowercase()
-
-        replacements.forEach { (k, v) ->
-            if (fixed.startsWith(k)) {
-                fixed = fixed.replaceFirst(k, v)
-            }
-        }
-
-        return fixed
-    }
-
     override fun solve(items: List<ItemStack>): List<Int> {
-
-        val colorName = when (color) {
-            DyeColor.LIGHT_GRAY -> "silver"
-            else -> color.name.replace("_", " ").lowercase()
-        }
+        val colorName = color.name.replace("_", " ")
 
         return items.mapIndexedNotNull { index, item ->
+            val nameMatches = item.hoverName.string.startsWith(colorName, true)
+            val itemMatches = when (color) {
+                DyeColor.BLACK  -> item.item == Items.INK_SAC
+                DyeColor.BLUE   -> item.item == Items.LAPIS_LAZULI
+                DyeColor.BROWN  -> item.item == Items.COCOA_BEANS
+                DyeColor.WHITE  -> item.item == Items.BONE_MEAL || item.item == Items.WHITE_WOOL
+                DyeColor.GREEN  -> item.item == Items.CACTUS
+                DyeColor.RED    -> item.item == Items.POPPY || item.item == Items.ROSE_BUSH
+                DyeColor.YELLOW -> item.item == Items.DANDELION
+                DyeColor.LIGHT_GRAY -> item.hoverName.string.startsWith("silver", true)
+                else -> false
+            }
 
-            val fixedName = fixName(item.hoverName.string)
-
-            if (
-                !item.hasGlint() &&
-                item.item != Items.BLACK_STAINED_GLASS_PANE &&
-                fixedName.startsWith(colorName, true)
-            ) index else null
+            if (!item.hasGlint() && item.item != Items.BLACK_STAINED_GLASS_PANE && (nameMatches || itemMatches)) index else null
         }
     }
 
